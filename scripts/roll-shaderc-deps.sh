da8223d2ef79f4bf3c49b312c563432da0695f9c
Ryan Harrison <rharrison@chromium.org>
2019-06-19 15:04:09 +0000

    Add script to assist in manually rolling shaderc and related DEPS
    Uses the functionality in depot_tools/roll-dep to generate a patch
    rolling all of the shaderc dependencies in DEPS to
    origin/master. Still will require manual testing to confirm this roll
    is good.
    
    Change-Id: Iadf6ee0f75ac84ce9bc7ec1ba82247dd96560088
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/8180
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

---
+++ scripts/roll-shaderc-deps.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/env bash
+
+# Copyright 2019 The Dawn Authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Attempts to roll the shaderc related entries in DEPS to origin/master and
+# creates a commit.
+#
+# Depends on roll-dep from depot_path being in PATH.
+
+# This script assumes it's parent directory is the repo root.
+repo_path=$(dirname "$0")/..
+
+glslang_dir="third_party/glslang/"
+spirv_cross_dir="third_party/spirv-cross/"
+spirv_headers_dir="third_party/spirv-headers/"
+spirv_tools_dir="third_party/SPIRV-Tools/"
+
+cd "$repo_path"
+
+roll-dep "${glslang_dir}" "${spirv_cross_dir}" "${spirv_headers_dir}" "${spirv_tools_dir}"
