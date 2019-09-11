2a2392073b5c7ea58aab69cc8fdccb42be81c6a2
Li, Hao <hao.x.li@intel.com>
2019-09-11 15:59:16 +0000

    Set point size value for vertex shader in OptionalVertexInputTest
    When pipeline topology is set to Point_List, PointSize must be written
    in vertex shader for Vulkan backend.
    
    BUG=dawn:146
    
    Change-Id: I0ea6aa937866d1985287d353f82d344100b41acd
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/11140
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Hao Li <hao.x.li@intel.com>

--- src/tests/end2end/VertexInputTests.cpp
+++ src/tests/end2end/VertexInputTests.cpp
@@ -534,6 +534,7 @@ TEST_P(OptionalVertexInputTest, Basic) {
             #version 450
             void main() {
                 gl_Position = vec4(0.0f, 0.0f, 0.0f, 1.0f);
+                gl_PointSize = 1.0;
             })");
 
     dawn::ShaderModule fsModule =
