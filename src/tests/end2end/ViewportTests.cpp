252d1eb47f23beb379bdbb47bf04ef4a71b660a8
Yunchao He <yunchao.he@intel.com>
2019-11-07 12:15:17 +0000

    Suppress viewport test
    BUG=dawn:205, dawn:257
    
    Change-Id: I319211832818e3aa865cbfc4b342328cad832230
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/13120
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

--- src/tests/end2end/ViewportTests.cpp
+++ src/tests/end2end/ViewportTests.cpp
@@ -383,9 +383,11 @@ TEST_P(ViewportTest, DoNotTruncateXAndY2) {
 // Width and height have fractions and they are greater than 3.5, which is the center of
 // point(3, 3). So point(3, 3) is covered by the bottom right triangle as usual.
 TEST_P(ViewportTest, DoNotTruncateWidthAndHeight) {
-    // Test failing on Intel devices (D3D, Vulkan and Metal) and D3D12.
+    // Test failing on many D3D12 backend and Intel devices.
+    // It also fails on Vulkan and GL backend on some devices.
     // See https://bugs.chromium.org/p/dawn/issues/detail?id=205
-    DAWN_SKIP_TEST_IF(IsIntel() || IsD3D12());
+    // See https://bugs.chromium.org/p/dawn/issues/detail?id=257
+    DAWN_SKIP_TEST_IF(IsIntel() || !IsMetal());
     ViewportParams viewport = {0.0, 0.0, 3.51, 3.51, 0.0, 1.0};
     TestInfo info = {viewport, TopLeftTriangleColor, BottomRightTriangleColor};
     DoTest(info);
