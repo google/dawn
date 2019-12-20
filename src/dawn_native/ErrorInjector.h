f58f69f66bacefb0aaf78e91b462b5db97cea391
Austin Eng <enga@chromium.org>
2019-12-20 15:52:20 +0000

    fuzzing: Add supportsErrorInjection option to DawnWireServerFuzzer
    This option will be used by backends that support error injection so
    that errors can be injected into a "clean" corpus to generate a seed
    corpus with good examples of injected error conditions.
    
    Bug: dawn:295
    Change-Id: I837acdde6dd4274adb56edf8e4307427f8d6333b
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/14681
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>

--- src/dawn_native/ErrorInjector.h
+++ src/dawn_native/ErrorInjector.h
@@ -26,12 +26,7 @@ namespace dawn_native {
         bool injected;
     };
 
-    void EnableErrorInjector();
-    void DisableErrorInjector();
-    void ClearErrorInjector();
-
     bool ErrorInjectorEnabled();
-    uint64_t AcquireErrorInjectorCallCount();
 
     bool ShouldInjectError();
 
@@ -47,8 +42,6 @@ namespace dawn_native {
         }
         return MaybeInjectError(errorTypes...);
     }
-
-    void InjectErrorAt(uint64_t index);
 
 }  // namespace dawn_native
 
