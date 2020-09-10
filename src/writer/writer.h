66dd683148dd81e52d4ce047ccac7372698c3913
David Neto <dneto@google.com>
2020-09-10 16:20:17 +0000

    Fix doxygen: don't describe return of void method
    Change-Id: Ib5795e44cbf23155dc2d2a7dcd6466e68dac4e2d
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/28281
    Reviewed-by: Sarah Mashayekhi <sarahmashay@google.com>
    Commit-Queue: Sarah Mashayekhi <sarahmashay@google.com>

--- src/writer/writer.h
+++ src/writer/writer.h
@@ -32,7 +32,6 @@ class Writer {
   const std::string& error() const { return error_; }
 
   /// Resets the generator
-  /// @returns nothing
   virtual void Reset() = 0;
 
   /// Converts the module into the desired format
