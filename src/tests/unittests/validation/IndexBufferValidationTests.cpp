8fc755b716e490bf29fd404fc10a75d56e72a483
Corentin Wallez <cwallez@chromium.org>
2020-11-26 12:22:35 +0000

    Port IndexBufferValidationTests to WGSL
    Bug: dawn:572
    Change-Id: If9deb74d334d0f2d5fbc7f78b6ceb646beb45826
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/33926
    Reviewed-by: Austin Eng <enga@chromium.org>
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>

--- src/tests/unittests/validation/IndexBufferValidationTests.cpp
+++ src/tests/unittests/validation/IndexBufferValidationTests.cpp
@@ -22,20 +22,17 @@ class IndexBufferValidationTest : public ValidationTest {
     protected:
     wgpu::RenderPipeline MakeTestPipeline(wgpu::IndexFormat format,
         wgpu::PrimitiveTopology primitiveTopology) {
-        wgpu::ShaderModule vsModule =
-            utils::CreateShaderModule(device, utils::SingleShaderStage::Vertex, R"(
-                #version 450
-                void main() {
-                    gl_Position = vec4(0, 0, 0, 1);
-                })");
+        wgpu::ShaderModule vsModule = utils::CreateShaderModuleFromWGSL(device, R"(
+            [[builtin(position)]] var<out> Position : vec4<f32>;
+            [[stage(vertex)]] fn main() -> void {
+                Position = vec4<f32>(0.0, 0.0, 0.0, 1.0);
+            })");
 
-        wgpu::ShaderModule fsModule =
-            utils::CreateShaderModule(device, utils::SingleShaderStage::Fragment, R"(
-                #version 450
-                layout(location = 0) out vec4 fragColor;
-                void main() {
-                    fragColor = vec4(0.0, 1.0, 0.0, 1.0);
-                })");
+        wgpu::ShaderModule fsModule = utils::CreateShaderModuleFromWGSL(device, R"(
+            [[location(0)]] var<out> fragColor : vec4<f32>;
+            [[stage(fragment)]] fn main() -> void {
+                fragColor = vec4<f32>(0.0, 1.0, 0.0, 1.0);
+            })");
 
         utils::ComboRenderPipelineDescriptor descriptor(device);
         descriptor.vertexStage.module = vsModule;
