2fe7335d34bc59edefd90a9d3f8683eb8ef83817
Jiawei Shao <jiawei.shao@intel.com>
2020-11-30 13:50:49 +0000

    Skip two MultisampledRenderingTest tests on D3D12 with debug layer
    This patch skips the following two dawn_end2end_tests on all D3D12
    backends when the D3D12 debug layer is enabled as they started to fail
    after the upgrade of Windows try bots.
    - MultisampledRenderingTest.ResolveOneMultisampledTextureTwice
    - MultisampledRenderingTest.ResolveInto2DArrayTexture
    
    See dawn:462 for more details.
    
    BUG=dawn:462
    
    Change-Id: I99c65ce250ba5b405a2143e6f866fd21ae33d63a
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/34281
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

--- src/tests/end2end/MultisampledRenderingTests.cpp
+++ src/tests/end2end/MultisampledRenderingTests.cpp
@@ -470,7 +470,7 @@ TEST_P(MultisampledRenderingTest, ResolveOneMultisampledTextureTwice) {
 // Test using a layer of a 2D texture as resolve target works correctly.
 TEST_P(MultisampledRenderingTest, ResolveIntoOneMipmapLevelOf2DTexture) {
     // TODO(dawn:462): Investigate backend validation failure.
-    DAWN_SKIP_TEST_IF(IsD3D12() && IsNvidia() && IsBackendValidationEnabled());
+    DAWN_SKIP_TEST_IF(IsD3D12() && IsBackendValidationEnabled());
 
     constexpr uint32_t kBaseMipLevel = 2;
 
@@ -508,7 +508,7 @@ TEST_P(MultisampledRenderingTest, ResolveIntoOneMipmapLevelOf2DTexture) {
 // Test using a level or a layer of a 2D array texture as resolve target works correctly.
 TEST_P(MultisampledRenderingTest, ResolveInto2DArrayTexture) {
     // TODO(dawn:462): Investigate backend validation failure.
-    DAWN_SKIP_TEST_IF(IsD3D12() && IsNvidia() && IsBackendValidationEnabled());
+    DAWN_SKIP_TEST_IF(IsD3D12() && IsBackendValidationEnabled());
 
     wgpu::TextureView multisampledColorView2 =
         CreateTextureForRenderAttachment(kColorFormat, kSampleCount).CreateView();
