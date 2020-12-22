84997c5ee97a89116eb552b6cb00c95f2950c8d1
Austin Eng <enga@chromium.org>
2020-12-22 19:56:48 +0000

    Update ViewportOrientationTests to use WGSL
    Bug: dawn:572
    Change-Id: Ifef4f75dcfd0077e3361ea0854cddf3e78e84a02
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/33887
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

--- src/tests/end2end/ViewportOrientationTests.cpp
+++ src/tests/end2end/ViewportOrientationTests.cpp
@@ -26,20 +26,17 @@ TEST_P(ViewportOrientationTests, OriginAt0x0) {
 
     utils::BasicRenderPass renderPass = utils::CreateBasicRenderPass(device, 2, 2);
 
-    wgpu::ShaderModule vsModule =
-        utils::CreateShaderModule(device, utils::SingleShaderStage::Vertex, R"(
-        #version 450
-        void main() {
-            gl_Position = vec4(-0.5f, 0.5f, 0.0f, 1.0f);
-            gl_PointSize = 1.0;
+    wgpu::ShaderModule vsModule = utils::CreateShaderModuleFromWGSL(device, R"(
+        [[builtin(position)]] var<out> Position : vec4<f32>;
+
+        [[stage(vertex)]] fn main() -> void {
+            Position = vec4<f32>(-0.5, 0.5, 0.0, 1.0);
         })");
 
-    wgpu::ShaderModule fsModule =
-        utils::CreateShaderModule(device, utils::SingleShaderStage::Fragment, R"(
-        #version 450
-        layout(location = 0) out vec4 fragColor;
-        void main() {
-            fragColor = vec4(0.0f, 1.0f, 0.0f, 1.0f);
+    wgpu::ShaderModule fsModule = utils::CreateShaderModuleFromWGSL(device, R"(
+        [[location(0)]] var<out> fragColor : vec4<f32>;
+        [[stage(fragment)]] fn main() -> void {
+            fragColor = vec4<f32>(0.0, 1.0, 0.0, 1.0);
         })");
 
     utils::ComboRenderPipelineDescriptor descriptor(device);
