2df77f4325841e667e6b9ff9921fd626496a3b64
Ryan Harrison <rharrison@chromium.org>
2021-01-26 21:52:45 +0000

    Removing old WebGPU SPIR-V behaviour test
    This test is attempting to confirm that behaviour from the SPIR-V
    WebGPU execution environment will occur. Given that the implementation
    of this spec has been removed from SPIRV-Tools and Tint handles this
    gracefully, the test will never succeed.
    
    Bug=dawn:57
    
    Change-Id: I0ee071c87a79947cbb63bcc816a7ab1314419b5e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/38900
    Auto-Submit: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: David Neto <dneto@google.com>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>

--- src/tests/unittests/validation/ShaderModuleValidationTests.cpp
+++ src/tests/unittests/validation/ShaderModuleValidationTests.cpp
@@ -54,45 +54,6 @@ TEST_F(ShaderModuleValidationTest, CreationSuccess) {
     utils::CreateShaderModuleFromASM(device, shader);
 }
 
-// Test case with a shader with OpUndef to test WebGPU-specific validation
-// TODO(cwallez@chromium.org): Disabled because of
-// https://bugs.chromium.org/p/dawn/issues/detail?id=57
-TEST_F(ShaderModuleValidationTest, DISABLED_OpUndef) {
-    const char* shader = R"(
-                   OpCapability Shader
-              %1 = OpExtInstImport "GLSL.std.450"
-                   OpMemoryModel Logical GLSL450
-                   OpEntryPoint Fragment %main "main" %fragColor
-                   OpExecutionMode %main OriginUpperLeft
-                   OpSource GLSL 450
-                   OpSourceExtension "GL_GOOGLE_cpp_style_line_directive"
-                   OpSourceExtension "GL_GOOGLE_include_directive"
-                   OpName %main "main"
-                   OpName %fragColor "fragColor"
-                   OpDecorate %fragColor Location 0
-           %void = OpTypeVoid
-              %3 = OpTypeFunction %void
-          %float = OpTypeFloat 32
-        %v4float = OpTypeVector %float 4
-    %_ptr_Output_v4float = OpTypePointer Output %v4float
-      %fragColor = OpVariable %_ptr_Output_v4float Output
-        %float_1 = OpConstant %float 1
-        %float_0 = OpConstant %float 0
-             %12 = OpConstantComposite %v4float %float_1 %float_0 %float_0 %float_1
-           %main = OpFunction %void None %3
-              %5 = OpLabel
-              %6 = OpUndef %v4float
-                   OpStore %fragColor %12
-                   OpReturn
-                   OpFunctionEnd)";
-
-    // Notice "%6 = OpUndef %v4float" above
-    ASSERT_DEVICE_ERROR(utils::CreateShaderModuleFromASM(device, shader));
-
-    std::string error = GetLastDeviceErrorMessage();
-    ASSERT_NE(error.find("OpUndef"), std::string::npos);
-}
-
 // Tests that if the output location exceeds kMaxColorAttachments the fragment shader will fail to
 // be compiled.
 TEST_F(ShaderModuleValidationTest, FragmentOutputLocationExceedsMaxColorAttachments) {
