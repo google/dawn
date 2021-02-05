05d9e2cde2792a0e95c44cacb11e443120720ad2
Austin Eng <enga@chromium.org>
2021-02-05 21:08:44 +0000

    Nuke the ClientMatches hack for same-device validation
    The wire now supports more than one device, and Chrome is updated
    to use the new code path. This fixes same-device validation for
    createReadyPipeline.
    
    Bug: dawn:565
    Change-Id: Id05001ed1a7e535690c87f535da6f72a0e794c59
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/40460
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>

--- generator/templates/dawn_wire/client/ApiProcs.cpp
+++ generator/templates/dawn_wire/client/ApiProcs.cpp
@@ -22,96 +22,15 @@
 #include <vector>
 
 namespace dawn_wire { namespace client {
-    namespace {
 
-        //* Outputs an rvalue that's the number of elements a pointer member points to.
-        {% macro member_length(member, accessor) -%}
-            {%- if member.length == "constant" -%}
-                {{member.constant_length}}
-            {%- else -%}
-                {{accessor}}{{as_varName(member.length.name)}}
-            {%- endif -%}
-        {%- endmacro %}
-
-        {% for type in by_category["object"] %}
-            DAWN_DECLARE_UNUSED bool ClientMatches(const Client* client, const {{as_cType(type.name)}} obj) {
-                return client == reinterpret_cast<const {{as_wireType(type)}}>(obj)->client;
-            }
-
-            DAWN_DECLARE_UNUSED bool ClientMatches(const Client* client, const {{as_cType(type.name)}} *const obj, uint32_t count = 1) {
-                ASSERT(count == 0 || obj != nullptr);
-                for (uint32_t i = 0; i < count; ++i) {
-                    if (!ClientMatches(client, obj[i])) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        {% endfor %}
-
-        bool ClientMatches(const Client* client, WGPUChainedStruct const* chainedStruct);
-
-        {% for type in by_category["structure"] if type.may_have_dawn_object %}
-            DAWN_DECLARE_UNUSED bool ClientMatches(const Client* client, const {{as_cType(type.name)}}& obj) {
-                {% if type.extensible %}
-                    if (!ClientMatches(client, obj.nextInChain)) {
-                        return false;
-                    }
-                {% endif %}
-                {% for member in type.members if member.type.may_have_dawn_object or member.type.category == "object" %}
-                    {% if member.optional %}
-                        if (obj.{{as_varName(member.name)}} != nullptr)
-                    {% endif %}
-                    {
-                        if (!ClientMatches(client, obj.{{as_varName(member.name)}}
-                            {%- if member.annotation != "value" and member.length != "strlen" -%}
-                                , {{member_length(member, "obj.")}}
-                            {%- endif -%})) {
-                            return false;
-                        }
-                    }
-                {% endfor %}
-                return true;
-            }
-
-            DAWN_DECLARE_UNUSED bool ClientMatches(const Client* client, const {{as_cType(type.name)}} *const obj, uint32_t count = 1) {
-                for (uint32_t i = 0; i < count; ++i) {
-                    if (!ClientMatches(client, obj[i])) {
-                        return false;
-                    }
-                }
-                return true;
-            }
-        {% endfor %}
-
-        bool ClientMatches(const Client* client, WGPUChainedStruct const* chainedStruct) {
-            while (chainedStruct != nullptr) {
-                switch (chainedStruct->sType) {
-                    {% for sType in types["s type"].values if sType.valid %}
-                        {% set CType = as_cType(sType.name) %}
-                        case {{as_cEnum(types["s type"].name, sType.name)}}: {
-                            {% if types[sType.name.get()].may_have_dawn_object %}
-                                if (!ClientMatches(client, reinterpret_cast<const {{CType}}*>(chainedStruct))) {
-                                    return false;
-                                }
-                            {% endif %}
-                            break;
-                        }
-                    {% endfor %}
-                    case WGPUSType_Invalid:
-                        break;
-                    default:
-                        dawn::WarningLog()
-                            << "All objects may not be from the same client. "
-                            << "Unknown sType " << chainedStruct->sType << " ignored.";
-                        break;
-                }
-                chainedStruct = chainedStruct->next;
-            }
-            return true;
-        }
-
-    }  // anonymous namespace
+    //* Outputs an rvalue that's the number of elements a pointer member points to.
+    {% macro member_length(member, accessor) -%}
+        {%- if member.length == "constant" -%}
+            {{member.constant_length}}
+        {%- else -%}
+            {{accessor}}{{as_varName(member.length.name)}}
+        {%- endif -%}
+    {%- endmacro %}
 
     //* Implementation of the client API functions.
     {% for type in by_category["object"] %}
@@ -130,49 +49,6 @@ namespace dawn_wire { namespace client {
                     , {{as_annotated_cType(arg)}}
                 {%- endfor -%}
             ) {
-                {% if len(method.arguments) > 0 %}
-                    {
-                        bool sameClient = true;
-                        auto self = reinterpret_cast<{{as_wireType(type)}}>(cSelf);
-                        Client* client = self->client;
-                        DAWN_UNUSED(client);
-
-                        do {
-                            {% for arg in method.arguments if arg.type.may_have_dawn_object or arg.type.category == "object" %}
-                                {% if arg.optional %}
-                                    if ({{as_varName(arg.name)}} != nullptr)
-                                {% endif %}
-                                {
-                                    if (!ClientMatches(client, {{as_varName(arg.name)}}
-                                        {%- if arg.annotation != "value" and arg.length != "strlen" -%}
-                                            , {{member_length(arg, "")}}
-                                        {%- endif -%})) {
-                                        sameClient = false;
-                                        break;
-                                    }
-                                }
-                            {% endfor %}
-                        } while (false);
-
-                        if (DAWN_UNLIKELY(!sameClient)) {
-                            reinterpret_cast<Device*>(client->GetDevice())->InjectError(WGPUErrorType_Validation,
-                                                "All objects must be from the same device.");
-                            {% if method.return_type.category == "object" %}
-                                // Allocate an object without registering it on the server. This is backed by a real allocation on
-                                // the client so commands can be sent with it. But because it's not allocated on the server, it will
-                                // be a fatal error to use it.
-                                auto self = reinterpret_cast<{{as_wireType(type)}}>(cSelf);
-                                auto* allocation = self->client->{{method.return_type.name.CamelCase()}}Allocator().New(self->client);
-                                return reinterpret_cast<{{as_cType(method.return_type.name)}}>(allocation->object.get());
-                            {% elif method.return_type.name.canonical_case() == "void" %}
-                                return;
-                            {% else %}
-                                return {};
-                            {% endif %}
-                        }
-                    }
-                {% endif %}
-
                 auto self = reinterpret_cast<{{as_wireType(type)}}>(cSelf);
                 {% if Suffix not in client_handwritten_commands %}
                     {{Suffix}}Cmd cmd;
---
+++ src/tests/unittests/validation/MultipleDeviceTests.cpp
@@ -0,0 +1,80 @@
+// Copyright 2021 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "tests/unittests/validation/ValidationTest.h"
+
+#include "tests/MockCallback.h"
+
+using namespace testing;
+
+class MultipleDeviceTest : public ValidationTest {};
+
+// Test that it is invalid to submit a command buffer created on a different device.
+TEST_F(MultipleDeviceTest, ValidatesSameDevice) {
+    wgpu::Device device2 = RegisterDevice(CreateTestDevice());
+    wgpu::CommandBuffer commandBuffer = device2.CreateCommandEncoder().Finish();
+
+    ASSERT_DEVICE_ERROR(device.GetQueue().Submit(1, &commandBuffer));
+}
+
+// Test that CreateReadyPipeline fails creation with an Error status if it uses
+// objects from a different device.
+TEST_F(MultipleDeviceTest, ValidatesSameDeviceCreateReadyPipeline) {
+    wgpu::ShaderModuleWGSLDescriptor wgslDesc = {};
+    wgslDesc.source = R"(
+        [[stage(compute)]] fn main() -> void {
+        }
+    )";
+
+    wgpu::ShaderModuleDescriptor shaderModuleDesc = {};
+    shaderModuleDesc.nextInChain = &wgslDesc;
+
+    // Base case: CreateReadyComputePipeline succeeds.
+    {
+        wgpu::ShaderModule shaderModule = device.CreateShaderModule(&shaderModuleDesc);
+
+        wgpu::ComputePipelineDescriptor pipelineDesc = {};
+        pipelineDesc.computeStage.module = shaderModule;
+        pipelineDesc.computeStage.entryPoint = "main";
+
+        StrictMock<MockCallback<WGPUCreateReadyComputePipelineCallback>> creationCallback;
+        EXPECT_CALL(creationCallback,
+                    Call(WGPUCreateReadyPipelineStatus_Success, NotNull(), _, this))
+            .WillOnce(WithArg<1>(Invoke(
+                [](WGPUComputePipeline pipeline) { wgpu::ComputePipeline::Acquire(pipeline); })));
+        device.CreateReadyComputePipeline(&pipelineDesc, creationCallback.Callback(),
+                                          creationCallback.MakeUserdata(this));
+
+        WaitForAllOperations(device);
+    }
+
+    // CreateReadyComputePipeline errors if the shader module is created on a different device.
+    {
+        wgpu::Device device2 = RegisterDevice(CreateTestDevice());
+        wgpu::ShaderModule shaderModule = device2.CreateShaderModule(&shaderModuleDesc);
+
+        wgpu::ComputePipelineDescriptor pipelineDesc = {};
+        pipelineDesc.computeStage.module = shaderModule;
+        pipelineDesc.computeStage.entryPoint = "main";
+
+        StrictMock<MockCallback<WGPUCreateReadyComputePipelineCallback>> creationCallback;
+        EXPECT_CALL(creationCallback,
+                    Call(WGPUCreateReadyPipelineStatus_Error, nullptr, _, this + 1))
+            .Times(1);
+        device.CreateReadyComputePipeline(&pipelineDesc, creationCallback.Callback(),
+                                          creationCallback.MakeUserdata(this + 1));
+
+        WaitForAllOperations(device);
+    }
+}
