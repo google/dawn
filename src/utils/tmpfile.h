a87fda92257b7a4f39bade39884499a7100575ae
Ben Clayton <bclayton@google.com>
2021-02-18 15:39:00 +0000

    Add tint::TmpFile
    TmpFile constructs a temporary file that can be written to, and is automatically deleted on destruction.
    
    Will be used to create a temporary source file for verifying generated HLSL and MSL against their shader compilers.
    
    Change-Id: Ieaa6f257b93f4f2193dafe6297603816f6964928
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/41941
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>

---
+++ src/utils/tmpfile.h
@@ -0,0 +1,74 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_UTILS_TMPFILE_H_
+#define SRC_UTILS_TMPFILE_H_
+
+#include <sstream>
+#include <string>
+
+namespace tint {
+namespace utils {
+
+/// TmpFile constructs a temporary file that can be written to, and is
+/// automatically deleted on destruction.
+class TmpFile {
+ public:
+  /// Constructor.
+  /// Creates a new temporary file which can be written to.
+  /// The temporary file will be automatically deleted on destruction.
+  TmpFile();
+
+  /// Destructor.
+  /// Deletes the temporary file.
+  ~TmpFile();
+
+  /// @return true if the temporary file was successfully created.
+  operator bool() { return !path_.empty(); }
+
+  /// @return the path to the temporary file
+  std::string Path() const { return path_; }
+
+  /// Opens the temporary file and appends |size| bytes from |data| to the end
+  /// of the temporary file. The temporary file is closed again before
+  /// returning, allowing other processes to open the file on operating systems
+  /// that require exclusive ownership of opened files.
+  /// @param data the data to write to the end of the file
+  /// @param size the number of bytes to write from data
+  /// @returns true on success, otherwise false
+  bool Append(const void* data, size_t size) const;
+
+  /// Appends the argument to the end of the file.
+  /// @param data the data to write to the end of the file
+  /// @return a reference to this TmpFile
+  template <typename T>
+  inline TmpFile& operator<<(T&& data) {
+    std::stringstream ss;
+    ss << data;
+    std::string str = ss.str();
+    Append(str.data(), str.size());
+    return *this;
+  }
+
+ private:
+  TmpFile(const TmpFile&) = delete;
+  TmpFile& operator=(const TmpFile&) = delete;
+
+  std::string path_;
+};
+
+}  // namespace utils
+}  // namespace tint
+
+#endif  //  SRC_UTILS_TMPFILE_H_
---
+++ src/utils/tmpfile_posix.cc
@@ -0,0 +1,56 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/utils/tmpfile.h"
+
+#include <stdio.h>
+#include <unistd.h>
+#include <cstdio>
+
+namespace tint {
+namespace utils {
+
+namespace {
+
+std::string TmpFilePath() {
+  char name[] = "tint_XXXXXX";
+  int file = mkstemp(name);
+  if (file != -1) {
+    close(file);
+    return name;
+  }
+  return "";
+}
+
+}  // namespace
+
+TmpFile::TmpFile() : path_(TmpFilePath()) {}
+
+TmpFile::~TmpFile() {
+  if (!path_.empty()) {
+    remove(path_.c_str());
+  }
+}
+
+bool TmpFile::Append(const void* data, size_t size) const {
+  if (auto* file = fopen(path_.c_str(), "ab")) {
+    fwrite(data, size, 1, file);
+    fclose(file);
+    return true;
+  }
+  return false;
+}
+
+}  // namespace utils
+}  // namespace tint
