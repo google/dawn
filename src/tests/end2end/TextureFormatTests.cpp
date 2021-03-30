682d764e85f9757e93f9f446f415ef574052e3b2
Ben Clayton <bclayton@google.com>
2021-03-30 16:21:07 +0000

    Disable TextureFormatTest for D3D12 + tint
    Bug: tint:689
    Change-Id: Ib006a2e726078426cd00479c35d47eecf9b3d601
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/46375
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

--- src/tests/end2end/TextureFormatTests.cpp
+++ src/tests/end2end/TextureFormatTests.cpp
@@ -115,6 +115,9 @@ class TextureFormatTest : public DawnTest {
   protected:
     void SetUp() {
         DawnTest::SetUp();
+
+        // TODO(crbug.com/tint/689): shaders compile, but produces unexpected output.
+        DAWN_SKIP_TEST_IF(IsD3D12() && HasToggleEnabled("use_tint_generator"));
     }
 
     // Structure containing all the information that tests need to know about the format.
