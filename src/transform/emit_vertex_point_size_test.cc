e6307e510009d965a452de544b1cefae64d5364f
James Price <jrprice@google.com>
2021-04-29 13:05:34 +0000

    transform/EmitVertexPointSize: Handle entry point parameters
    Generate a new struct that contains members of the original return
    type with the point size appended to it, and replace return statements
    as necessary.
    
    The SPIR-V sanitizer then special-cases this builtin when handling
    entry point IO to always use a RHS which is a literal.
    
    Fixed: tint:732
    Change-Id: Id718632a5e671f3e7c82a304f5bc1fc223a6c8ee
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/49440
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: James Price <jrprice@google.com>
    Auto-Submit: James Price <jrprice@google.com>

--- src/transform/emit_vertex_point_size_test.cc
+++ src/transform/emit_vertex_point_size_test.cc
@@ -29,7 +29,6 @@ fn non_entry_a() {
 
 [[stage(vertex)]]
 fn entry() -> [[builtin(position)]] vec4<f32> {
-  var builtin_assignments_should_happen_before_this : f32;
   return vec4<f32>();
 }
 
@@ -38,19 +37,271 @@ fn non_entry_b() {
 )";
 
   auto* expect = R"(
-[[builtin(pointsize)]] var<out> tint_pointsize : f32;
+struct tint_symbol {
+  [[builtin(position)]]
+  position : vec4<f32>;
+  [[builtin(pointsize)]]
+  tint_pointsize : f32;
+};
 
 fn non_entry_a() {
 }
 
 [[stage(vertex)]]
-fn entry() -> [[builtin(position)]] vec4<f32> {
-  tint_pointsize = 1.0;
-  var builtin_assignments_should_happen_before_this : f32;
-  return vec4<f32>();
+fn entry() -> tint_symbol {
+  return tint_symbol(vec4<f32>(), 1.0);
 }
 
 fn non_entry_b() {
+}
+)";
+
+  auto got = Run<EmitVertexPointSize>(src);
+
+  EXPECT_EQ(expect, str(got));
+}
+
+TEST_F(EmitVertexPointSizeTest, VertexStageBasic_Struct) {
+  auto* src = R"(
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+fn non_entry_a() {
+}
+
+[[stage(vertex)]]
+fn entry() -> VertexOut {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return output;
+}
+
+fn non_entry_b() {
+}
+)";
+
+  auto* expect = R"(
+struct tint_symbol {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+  [[builtin(pointsize)]]
+  tint_pointsize : f32;
+};
+
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+fn non_entry_a() {
+}
+
+[[stage(vertex)]]
+fn entry() -> tint_symbol {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return tint_symbol(output.pos, output.col, 1.0);
+}
+
+fn non_entry_b() {
+}
+)";
+
+  auto got = Run<EmitVertexPointSize>(src);
+
+  EXPECT_EQ(expect, str(got));
+}
+
+// Make sure we capture the function return value in a temporary instead of
+// re-evaluating it multiple times.
+TEST_F(EmitVertexPointSizeTest, VertexStage_ReturnStructFromFunctionCall) {
+  auto* src = R"(
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+fn foo() -> VertexOut {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return output;
+}
+
+[[stage(vertex)]]
+fn entry() -> VertexOut {
+  return foo();
+}
+)";
+
+  auto* expect = R"(
+struct tint_symbol {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+  [[builtin(pointsize)]]
+  tint_pointsize : f32;
+};
+
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+fn foo() -> VertexOut {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return output;
+}
+
+[[stage(vertex)]]
+fn entry() -> tint_symbol {
+  let tint_symbol_1 : VertexOut = foo();
+  return tint_symbol(tint_symbol_1.pos, tint_symbol_1.col, 1.0);
+}
+)";
+
+  auto got = Run<EmitVertexPointSize>(src);
+
+  EXPECT_EQ(expect, str(got));
+}
+
+TEST_F(EmitVertexPointSizeTest, VertexStage_MultipleReturnStatements) {
+  auto* src = R"(
+[[stage(vertex)]]
+fn entry([[location(0)]] toggle : u32) -> [[builtin(position)]] vec4<f32> {
+  if (toggle == 1u) {
+    return vec4<f32>(0.5, 0.5, 0.5, 0.5);
+  }
+  return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+}
+)";
+
+  auto* expect = R"(
+struct tint_symbol {
+  [[builtin(position)]]
+  position : vec4<f32>;
+  [[builtin(pointsize)]]
+  tint_pointsize : f32;
+};
+
+[[stage(vertex)]]
+fn entry([[location(0)]] toggle : u32) -> tint_symbol {
+  if ((toggle == 1u)) {
+    return tint_symbol(vec4<f32>(0.5, 0.5, 0.5, 0.5), 1.0);
+  }
+  return tint_symbol(vec4<f32>(1.0, 1.0, 1.0, 1.0), 1.0);
+}
+)";
+
+  auto got = Run<EmitVertexPointSize>(src);
+
+  EXPECT_EQ(expect, str(got));
+}
+
+// Test that we re-use generated structures when we've seen the original return
+// type before.
+TEST_F(EmitVertexPointSizeTest, VertexStage_MultipleShaders) {
+  auto* src = R"(
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+[[stage(vertex)]]
+fn entry1() -> [[builtin(position)]] vec4<f32> {
+  return vec4<f32>();
+}
+
+[[stage(vertex)]]
+fn entry2() -> [[builtin(position)]] vec4<f32> {
+  return vec4<f32>(1.0, 1.0, 1.0, 1.0);
+}
+
+[[stage(vertex)]]
+fn entry3() -> VertexOut {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return output;
+}
+
+[[stage(vertex)]]
+fn entry4() -> VertexOut {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.75;
+  return output;
+}
+
+)";
+
+  auto* expect = R"(
+struct tint_symbol {
+  [[builtin(position)]]
+  position : vec4<f32>;
+  [[builtin(pointsize)]]
+  tint_pointsize : f32;
+};
+
+struct tint_symbol_1 {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+  [[builtin(pointsize)]]
+  tint_pointsize_1 : f32;
+};
+
+struct VertexOut {
+  [[builtin(position)]]
+  pos : vec4<f32>;
+  [[location(0)]]
+  col : f32;
+};
+
+[[stage(vertex)]]
+fn entry1() -> tint_symbol {
+  return tint_symbol(vec4<f32>(), 1.0);
+}
+
+[[stage(vertex)]]
+fn entry2() -> tint_symbol {
+  return tint_symbol(vec4<f32>(1.0, 1.0, 1.0, 1.0), 1.0);
+}
+
+[[stage(vertex)]]
+fn entry3() -> tint_symbol_1 {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.5;
+  return tint_symbol_1(output.pos, output.col, 1.0);
+}
+
+[[stage(vertex)]]
+fn entry4() -> tint_symbol_1 {
+  var output : VertexOut;
+  output.pos = vec4<f32>();
+  output.col = 0.75;
+  return tint_symbol_1(output.pos, output.col, 1.0);
 }
 )";
 
@@ -87,21 +338,34 @@ fn compute_entry() {
 
 TEST_F(EmitVertexPointSizeTest, AttemptSymbolCollision) {
   auto* src = R"(
+struct VertexOut {
+  [[builtin(position)]]
+  tint_pointsize : vec4<f32>;
+};
+
 [[stage(vertex)]]
-fn entry() -> [[builtin(position)]] vec4<f32> {
-  var tint_pointsize : f32;
-  return vec4<f32>();
+fn entry() -> VertexOut {
+  return VertexOut(vec4<f32>());
 }
 )";
 
   auto* expect = R"(
-[[builtin(pointsize)]] var<out> tint_pointsize_1 : f32;
+struct tint_symbol {
+  [[builtin(position)]]
+  tint_pointsize : vec4<f32>;
+  [[builtin(pointsize)]]
+  tint_pointsize_1 : f32;
+};
+
+struct VertexOut {
+  [[builtin(position)]]
+  tint_pointsize : vec4<f32>;
+};
 
 [[stage(vertex)]]
-fn entry() -> [[builtin(position)]] vec4<f32> {
-  tint_pointsize_1 = 1.0;
-  var tint_pointsize : f32;
-  return vec4<f32>();
+fn entry() -> tint_symbol {
+  let tint_symbol_1 : VertexOut = VertexOut(vec4<f32>());
+  return tint_symbol(tint_symbol_1.tint_pointsize, 1.0);
 }
 )";
 
