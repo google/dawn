ad19a63c5431710377c426957c672692ffdabb5e
Ben Clayton <bclayton@google.com>
2021-05-10 19:45:46 +0000

    Add sem::Reference
    Bug: tint:727
    Change-Id: I904d33b6c0678f478b4687fcc08589dc3cfd379d
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/50541
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: James Price <jrprice@google.com>

---
+++ src/sem/reference_type.cc
@@ -0,0 +1,48 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "src/sem/reference_type.h"
+
+#include "src/program_builder.h"
+
+TINT_INSTANTIATE_TYPEINFO(tint::sem::Reference);
+
+namespace tint {
+namespace sem {
+
+Reference::Reference(const Type* subtype, ast::StorageClass storage_class)
+    : subtype_(subtype), storage_class_(storage_class) {}
+
+std::string Reference::type_name() const {
+  std::ostringstream out;
+  out << "__ref_" << storage_class_ << subtype_->type_name();
+  return out.str();
+}
+
+std::string Reference::FriendlyName(const SymbolTable& symbols) const {
+  std::ostringstream out;
+  out << "ref<";
+  if (storage_class_ != ast::StorageClass::kNone) {
+    out << storage_class_ << ", ";
+  }
+  out << subtype_->FriendlyName(symbols) << ">";
+  return out.str();
+}
+
+Reference::Reference(Reference&&) = default;
+
+Reference::~Reference() = default;
+
+}  // namespace sem
+}  // namespace tint
--- src/sem/reference_type_test.cc
+++ src/sem/reference_type_test.cc
@@ -1,4 +1,4 @@
-// Copyright 2020 The Tint Authors.
+// Copyright 2021 The Tint Authors.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,35 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "src/sem/access_control_type.h"
+#include "src/sem/reference_type.h"
 #include "src/sem/test_helper.h"
-#include "src/sem/texture_type.h"
 
 namespace tint {
 namespace sem {
 namespace {
 
-using PointerTest = TestHelper;
+using ReferenceTest = TestHelper;
 
-TEST_F(PointerTest, Creation) {
-  auto* r = create<Pointer>(create<I32>(), ast::StorageClass::kStorage);
+TEST_F(ReferenceTest, Creation) {
+  auto* r = create<Reference>(create<I32>(), ast::StorageClass::kStorage);
   EXPECT_TRUE(r->type()->Is<sem::I32>());
   EXPECT_EQ(r->storage_class(), ast::StorageClass::kStorage);
 }
 
-TEST_F(PointerTest, TypeName) {
-  auto* r = create<Pointer>(create<I32>(), ast::StorageClass::kWorkgroup);
-  EXPECT_EQ(r->type_name(), "__ptr_workgroup__i32");
+TEST_F(ReferenceTest, TypeName) {
+  auto* r = create<Reference>(create<I32>(), ast::StorageClass::kWorkgroup);
+  EXPECT_EQ(r->type_name(), "__ref_workgroup__i32");
 }
 
-TEST_F(PointerTest, FriendlyNameWithStorageClass) {
-  auto* r = create<Pointer>(ty.i32(), ast::StorageClass::kWorkgroup);
-  EXPECT_EQ(r->FriendlyName(Symbols()), "ptr<workgroup, i32>");
+TEST_F(ReferenceTest, FriendlyNameWithStorageClass) {
+  auto* r = create<Reference>(ty.i32(), ast::StorageClass::kWorkgroup);
+  EXPECT_EQ(r->FriendlyName(Symbols()), "ref<workgroup, i32>");
 }
 
-TEST_F(PointerTest, FriendlyNameWithoutStorageClass) {
-  auto* r = create<Pointer>(ty.i32(), ast::StorageClass::kNone);
-  EXPECT_EQ(r->FriendlyName(Symbols()), "ptr<i32>");
+TEST_F(ReferenceTest, FriendlyNameWithoutStorageClass) {
+  auto* r = create<Reference>(ty.i32(), ast::StorageClass::kNone);
+  EXPECT_EQ(r->FriendlyName(Symbols()), "ref<i32>");
 }
 
 }  // namespace
