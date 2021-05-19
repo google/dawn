594075a2f0dbffc1966e9396b3eb771d274fa4b5
Antonio Maiorano <amaiorano@google.com>
2021-05-19 08:13:28 +0000

    Add DuplicateStorageStruct transform
    This transform is currently required by Dawn for it's GLSL backend so
    that SPIRV-Cross does not end up renaming the structures internally,
    which it does to fix aliasing problems. We can remove this transform
    if/once Dawn using binding numbers rather than names for uniform/storage
    buffer data.
    
    Bug: tint:386
    Bug: tint:808
    
    CloneContext: allow insert before/after of a node marked for removal
    Change-Id: I3ff3b37bca62db07d5c759250dd4777e279b7a4f
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/51403
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>

--- src/transform/spirv.cc
+++ src/transform/spirv.cc
@@ -26,6 +26,7 @@
 #include "src/sem/statement.h"
 #include "src/sem/struct.h"
 #include "src/sem/variable.h"
+#include "src/transform/duplicate_storage_structs.h"
 #include "src/transform/external_texture_transform.h"
 #include "src/transform/manager.h"
 
@@ -40,6 +41,7 @@ Spirv::~Spirv() = default;
 Output Spirv::Run(const Program* in, const DataMap& data) {
   Manager manager;
   manager.Add<ExternalTextureTransform>();
+  manager.Add<DuplicateStorageStructs>();
   auto transformedInput = manager.Run(in, data);
 
   auto* cfg = data.Get<Config>();
