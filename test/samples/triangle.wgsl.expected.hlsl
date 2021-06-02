a7a23eaa9e9623357563e403bc6ef42e8fd27b90
James Price <jrprice@google.com>
2021-06-02 20:53:14 +0000

    test: Fix type of vertex_index builtin
    This should be a u32, but we are missing validation rules for builtin
    types (opened crbug.com/tint/861 for this). This meant that we were
    generating invalid HLSL for this test.
    
    Change-Id: Ib2189d98e5e515e41374372c0f8963df04eb1b01
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/53001
    Auto-Submit: James Price <jrprice@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Commit-Queue: Antonio Maiorano <amaiorano@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>

--- test/samples/triangle.wgsl.expected.hlsl
+++ test/samples/triangle.wgsl.expected.hlsl
@@ -1,5 +1,5 @@
 struct tint_symbol_1 {
-  int VertexIndex : SV_VertexID;
+  uint VertexIndex : SV_VertexID;
 };
 struct tint_symbol_2 {
   float4 value : SV_Position;
@@ -10,7 +10,7 @@ struct tint_symbol_3 {
 
 static const float2 pos[3] = {float2(0.0f, 0.5f), float2(-0.5f, -0.5f), float2(0.5f, -0.5f)};
 tint_symbol_2 vtx_main(tint_symbol_1 tint_symbol) {
-  const int VertexIndex = tint_symbol.VertexIndex;
+  const uint VertexIndex = tint_symbol.VertexIndex;
   const tint_symbol_2 tint_symbol_4 = {float4(pos[VertexIndex], 0.0f, 1.0f)};
   return tint_symbol_4;
 }
