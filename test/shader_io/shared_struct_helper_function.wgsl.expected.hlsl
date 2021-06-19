a865b375aafe0f803b7d7e7477cc1af0e2c4c5d3
James Price <jrprice@google.com>
2021-06-19 01:12:45 +0000

    test: Add missing stage attributes to IO test
    Change-Id: I7f69de9c3b6fe226e0688f56a8d076dc6ec4a81f
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/55281
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Commit-Queue: James Price <jrprice@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>

--- test/shader_io/shared_struct_helper_function.wgsl.expected.hlsl
+++ test/shader_io/shared_struct_helper_function.wgsl.expected.hlsl
@@ -1,22 +1,31 @@
-[numthreads(1, 1, 1)]
-void unused_entry_point() {
-  return;
-}
-
 struct VertexOutput {
   float4 pos;
   int loc0;
 };
 
 VertexOutput foo(float x) {
-  const VertexOutput tint_symbol = {float4(x, x, x, 1.0f), 42};
-  return tint_symbol;
+  const VertexOutput tint_symbol_4 = {float4(x, x, x, 1.0f), 42};
+  return tint_symbol_4;
 }
 
-VertexOutput vert_main1() {
-  return foo(0.5f);
+struct tint_symbol {
+  int loc0 : TEXCOORD0;
+  float4 pos : SV_Position;
+};
+
+tint_symbol vert_main1() {
+  const VertexOutput tint_symbol_1 = foo(0.5f);
+  const tint_symbol tint_symbol_5 = {tint_symbol_1.loc0, tint_symbol_1.pos};
+  return tint_symbol_5;
 }
 
-VertexOutput vert_main2() {
-  return foo(0.25f);
+struct tint_symbol_2 {
+  int loc0 : TEXCOORD0;
+  float4 pos : SV_Position;
+};
+
+tint_symbol_2 vert_main2() {
+  const VertexOutput tint_symbol_3 = foo(0.25f);
+  const tint_symbol_2 tint_symbol_6 = {tint_symbol_3.loc0, tint_symbol_3.pos};
+  return tint_symbol_6;
 }
