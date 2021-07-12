f07ad6733b6adf84fab266d0d7c5f0ce56d592b7
James Price <jrprice@google.com>
2021-07-12 11:44:51 +0000

    ast: Add InvariantDecoration
    Bug: tint:772
    Change-Id: Icd1892a1affed10af8e7b90257129fa54360815e
    Reviewed-on: https://dawn-review.googlesource.com/c/tint/+/57640
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Kokoro: Ben Clayton <bclayton@google.com>

---
+++ src/ast/invariant_decoration.h
@@ -0,0 +1,55 @@
+// Copyright 2021 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_AST_INVARIANT_DECORATION_H_
+#define SRC_AST_INVARIANT_DECORATION_H_
+
+#include <string>
+
+#include "src/ast/decoration.h"
+
+namespace tint {
+namespace ast {
+
+/// The invariant attribute
+class InvariantDecoration : public Castable<InvariantDecoration, Decoration> {
+ public:
+  /// constructor
+  /// @param program_id the identifier of the program that owns this node
+  /// @param source the source of this decoration
+  InvariantDecoration(ProgramID program_id, const Source& source);
+  ~InvariantDecoration() override;
+
+  /// @returns the WGSL name for the decoration
+  std::string name() const override;
+
+  /// Outputs the decoration to the given stream
+  /// @param sem the semantic info for the program
+  /// @param out the stream to write to
+  /// @param indent number of spaces to indent the node when writing
+  void to_str(const sem::Info& sem,
+              std::ostream& out,
+              size_t indent) const override;
+
+  /// Clones this node and all transitive child nodes using the `CloneContext`
+  /// `ctx`.
+  /// @param ctx the clone context
+  /// @return the newly cloned node
+  InvariantDecoration* Clone(CloneContext* ctx) const override;
+};
+
+}  // namespace ast
+}  // namespace tint
+
+#endif  // SRC_AST_INVARIANT_DECORATION_H_
