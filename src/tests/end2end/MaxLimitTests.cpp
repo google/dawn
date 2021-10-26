3c786cd418f692b5e0a12adfb35b7b250eb579ca
Austin Eng <enga@chromium.org>
2021-10-26 18:13:46 +0000

    Revert "Reland "Populate some D3D12 limits from the backend""
    This reverts commit 167d299916e3399765e62e52e3a28cacf2696b45.
    
    Reason for revert: Still failing the roll because of OOM on x86.
    
    Original change's description:
    > Reland "Populate some D3D12 limits from the backend"
    >
    > This is a reland of aa8fcfc64b10c5961441bf63da08cd54343bd415
    > It changes MaxLimitTests to use at most a 512MB buffer on 32-bit
    > platforms.
    >
    > Original change's description:
    > > Populate some D3D12 limits from the backend
    > >
    > > Also bumps the required D3D feature level to 11.1
    > >
    > > Bug: dawn:685
    > > Change-Id: I40bc3a162e0aee596d61118ba0dfe0bf9cb60d93
    > > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/65120
    > > Commit-Queue: Austin Eng <enga@chromium.org>
    > > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    >
    > Bug: dawn:685
    > Change-Id: I2e1df5f7ac0c9bbb6476ca2e1964a9af4afd89b6
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/67145
    > Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    > Reviewed-by: Rafael Cintron <rafael.cintron@microsoft.com>
    > Commit-Queue: Austin Eng <enga@chromium.org>
    
    TBR=cwallez@chromium.org,enga@chromium.org,rafael.cintron@microsoft.com,dawn-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    Change-Id: Ibad5ac7f28015da6a7334ba074aa975198591318
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: dawn:685
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/67564
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>

--- src/tests/end2end/MaxLimitTests.cpp
+++ src/tests/end2end/MaxLimitTests.cpp
@@ -111,13 +111,9 @@ TEST_P(MaxLimitTests, MaxBufferBindingSize) {
                 // because allocating the buffer for zero-initialization fails.
                 maxBufferBindingSize =
                     std::min(maxBufferBindingSize, uint64_t(2) * 1024 * 1024 * 1024);
-                // With WARP or on 32-bit platforms, such large buffer allocations often fail.
-#ifndef DAWN_PLATFORM_32BIT
-                if (IsWARP())
-#endif
-                {
+                if (IsWARP()) {
                     maxBufferBindingSize =
-                        std::min(maxBufferBindingSize, uint64_t(512) * 1024 * 1024);
+                        std::min(maxBufferBindingSize, uint64_t(1) * 1024 * 1024 * 1024);
                 }
                 shader = R"(
                   [[block]] struct Buf {
