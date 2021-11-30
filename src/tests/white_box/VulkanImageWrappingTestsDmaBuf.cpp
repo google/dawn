ba276a4f554cbe11702f8def449991c527fc2da6
jchen10 <jie.a.chen@intel.com>
2021-11-30 12:40:59 +0000

    Bug fixes for VulkanImageWrappingTestsDmaBuf
    VulkanImageWrappingTestsOpaqueFD already has the same fixes, but
    sadly didn't get applied to VulkanImageWrappingTestsDmaBuf.
    DawnTest::Setup() and TearDown() are missing so the device isn't
    initialized.
    The expected values in CopyTextureToBufferSrcSync are wrong.
    
    Bug: dawn:1210
    Change-Id: I3810a626d840573ab7c1fa75ba7ca239e7288c3e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/71245
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jie A Chen <jie.a.chen@intel.com>

--- src/tests/white_box/VulkanImageWrappingTestsDmaBuf.cpp
+++ src/tests/white_box/VulkanImageWrappingTestsDmaBuf.cpp
@@ -40,6 +40,7 @@ namespace dawn_native { namespace vulkan {
 
           public:
             void SetUp() override {
+                DawnTest::SetUp();
                 DAWN_TEST_UNSUPPORTED_IF(UsesWire());
 
                 gbmDevice = CreateGbmDevice();
@@ -60,11 +61,15 @@ namespace dawn_native { namespace vulkan {
             }
 
             void TearDown() override {
-                if (UsesWire())
+                if (UsesWire()) {
+                    DawnTest::TearDown();
                     return;
+                }
 
                 gbm_bo_destroy(defaultGbmBo);
                 gbm_device_destroy(gbmDevice);
+
+                DawnTest::TearDown();
             }
 
             gbm_device* CreateGbmDevice() {
@@ -554,7 +559,7 @@ namespace dawn_native { namespace vulkan {
         queue.Submit(1, &commands);
 
         // Verify |copyDstBuffer| sees changes from |secondDevice|
-        uint32_t expected = 1;
+        uint32_t expected = 0x04030201;
         EXPECT_BUFFER_U32_EQ(expected, copyDstBuffer, 0);
 
         IgnoreSignalSemaphore(deviceWrappedTexture);
