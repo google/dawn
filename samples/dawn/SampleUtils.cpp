a6750751c735fc70aa40617bb4a9123e875c5bd8
Ben Clayton <bclayton@google.com>
2022-02-04 18:35:55 +0000

    tint->dawn: Move examples -> samples/dawn
    Bug: dawn:1275
    Change-Id: I4824aaabc2c2b55a609a6e1370959394b93463b8
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/79103
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Ben Clayton <bclayton@google.com>

--- samples/dawn/SampleUtils.cpp
+++ samples/dawn/SampleUtils.cpp
@@ -14,20 +14,19 @@
 
 #include "SampleUtils.h"
 
+#include "GLFW/glfw3.h"
 #include "dawn/common/Assert.h"
 #include "dawn/common/Log.h"
 #include "dawn/common/Platform.h"
 #include "dawn/common/SystemUtils.h"
+#include "dawn/dawn_proc.h"
+#include "dawn/dawn_wsi.h"
+#include "dawn/native/DawnNative.h"
 #include "dawn/utils/BackendBinding.h"
 #include "dawn/utils/GLFWUtils.h"
 #include "dawn/utils/TerribleCommandBuffer.h"
 #include "dawn/wire/WireClient.h"
 #include "dawn/wire/WireServer.h"
-
-#include <dawn/dawn_proc.h>
-#include <dawn/dawn_wsi.h>
-#include <dawn/native/DawnNative.h>
-#include "GLFW/glfw3.h"
 
 #include <algorithm>
 #include <cstring>
