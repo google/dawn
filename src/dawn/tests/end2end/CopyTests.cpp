7ee6bf3dda9d0c7da08b8b436cfcf477ac4bd787
Yunchao He <yunchao.he@intel.com>
2022-04-05 16:48:33 +0000

    Disable a flaky T2T copy test on Intel Windows D3D12
    Bug: chromium:1312066
    Change-Id: Ia31496cdf9178a860e44503d81eda5be10c3d4e3
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/85681
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Austin Eng <enga@chromium.org>

--- src/dawn/tests/end2end/CopyTests.cpp
+++ src/dawn/tests/end2end/CopyTests.cpp
@@ -2179,6 +2179,9 @@ TEST_P(CopyTests_T2T, CopyFromNonZeroMipLevelWithTexelBlockSizeLessThan4Bytes) {
     // try bots.
     DAWN_SUPPRESS_TEST_IF(IsVulkan() && IsWindows() && IsIntel());
 
+    // This test also fails on D3D12 on Intel Windows. See http://crbug.com/1312066 for details.
+    DAWN_SUPPRESS_TEST_IF(IsD3D12() && IsWindows() && IsIntel());
+
     constexpr std::array<wgpu::TextureFormat, 11> kFormats = {
         {wgpu::TextureFormat::RG8Sint, wgpu::TextureFormat::RG8Uint, wgpu::TextureFormat::RG8Snorm,
          wgpu::TextureFormat::RG8Unorm, wgpu::TextureFormat::R16Float, wgpu::TextureFormat::R16Sint,
