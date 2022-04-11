a3b3b431622bd45fbf5b46fdba6acb83809d9d46
dan sinclair <dsinclair@chromium.org>
2022-04-11 14:46:11 +0000

    Fix build/include lint in samples/
    This CL updates the SampleUtils file to use the full path to the header.
    This required adding the root path into the include directories for the
    sample utils project in CMake.
    
    Bug: dawn:1339
    Change-Id: If6abb18a2eefa19ee56b6f889080da67f4a953e3
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/86211
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Dan Sinclair <dsinclair@chromium.org>
    Auto-Submit: Dan Sinclair <dsinclair@chromium.org>

--- samples/dawn/SampleUtils.cpp
+++ samples/dawn/SampleUtils.cpp
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include "SampleUtils.h"
+#include "samples/dawn/SampleUtils.h"
 
 #include "GLFW/glfw3.h"
 #include "dawn/common/Assert.h"
