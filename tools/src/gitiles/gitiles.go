ac068c9a31f7c3e8beacb32f985d233a9f8c49e4
Ben Clayton <bclayton@google.com>
2022-04-12 18:01:51 +0000

    tools: Add gitiles package
    Wraps the luci package to make it a bit friendlier.
    
    Bug: dawn:1342
    Change-Id: I70bf01b7ef5fcf210a2ff7ccafa49ea0720ae125
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/86526
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Ben Clayton <bclayton@chromium.org>

---
+++ tools/src/gitiles/gitiles.go
@@ -0,0 +1,72 @@
+// Copyright 2022 The Tint Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// Package gitiles provides helpers for interfacing with gitiles
+package gitiles
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"go.chromium.org/luci/common/api/gitiles"
+	gpb "go.chromium.org/luci/common/proto/gitiles"
+)
+
+// Gitiles is the client to communicate with Gitiles.
+type Gitiles struct {
+	client  gpb.GitilesClient
+	project string
+}
+
+// New creates a client to communicate with Gitiles, for the given host and
+// project.
+func New(ctx context.Context, host, project string) (*Gitiles, error) {
+	client, err := gitiles.NewRESTClient(http.DefaultClient, host, false)
+	if err != nil {
+		return nil, err
+	}
+	return &Gitiles{client, project}, nil
+}
+
+// Hash returns the git hash of the object with the given 'committish' reference.
+func (g *Gitiles) Hash(ctx context.Context, ref string) (string, error) {
+	res, err := g.client.Log(ctx, &gpb.LogRequest{
+		Project:    g.project,
+		Committish: ref,
+		PageSize:   1,
+	})
+	if err != nil {
+		return "", err
+	}
+	log := res.GetLog()
+	if len(log) == 0 {
+		return "", fmt.Errorf("gitiles returned log was empty")
+	}
+	return log[0].Id, nil
+}
+
+// DownloadFile downloads a single file with the given project-relative path at
+// the given reference.
+func (g *Gitiles) DownloadFile(ctx context.Context, ref, path string) (string, error) {
+	res, err := g.client.DownloadFile(ctx, &gpb.DownloadFileRequest{
+		Project:    g.project,
+		Committish: ref,
+		Path:       path,
+	})
+	if err != nil {
+		return "", err
+	}
+	return res.GetContents(), nil
+}
--- tools/src/go.mod
+++ tools/src/go.mod
@@ -9,6 +9,7 @@ require (
 	github.com/google/go-cmp v0.5.6
 	github.com/sergi/go-diff v1.2.0
 	github.com/shirou/gopsutil v3.21.11+incompatible
+	go.chromium.org/luci v0.0.0-20220412023008-ab2409fe739a
 	golang.org/x/net v0.0.0-20220403103023-749bd193bc2b
 )
 
@@ -17,15 +18,23 @@ require (
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.3.1 // indirect
 	github.com/go-ole/go-ole v1.2.6 // indirect
+	github.com/golang/mock v1.6.0 // indirect
+	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/jbenet/go-context v0.0.0-20150711004518-d14ea06fba99 // indirect
+	github.com/julienschmidt/httprouter v1.3.0 // indirect
+	github.com/klauspost/compress v1.13.5 // indirect
 	github.com/mattn/go-colorable v0.1.9 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
 	github.com/mitchellh/go-homedir v1.1.0 // indirect
 	github.com/tklauser/go-sysconf v0.3.10 // indirect
 	github.com/tklauser/numcpus v0.4.0 // indirect
 	github.com/yusufpapurcu/wmi v1.2.2 // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220128215802-99c3d69c2c27 // indirect
-	golang.org/x/xerrors v0.0.0-20200804184101-5ec99f83aff1 // indirect
+	golang.org/x/text v0.3.7 // indirect
+	google.golang.org/genproto v0.0.0-20220216160803-4663080d8bc8 // indirect
+	google.golang.org/grpc v1.44.0 // indirect
+	google.golang.org/protobuf v1.27.1 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 )
