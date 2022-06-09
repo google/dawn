5fb759f6e76a827f19ed6665b77aaeaa4cc4bbf2
Stephen White <senorblanco@chromium.org>
2022-06-09 02:10:32 +0000

    OpenGL: factor out Adapter (GL) into its own file.
    Bug: dawn:810
    Change-Id: I579e5c7cd610fc660b7480673a93cffdf725ef41
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/93200
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Stephen White <senorblanco@chromium.org>

---
+++ src/dawn/native/opengl/AdapterGL.h
@@ -0,0 +1,45 @@
+// Copyright 2022 The Dawn Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef SRC_DAWN_NATIVE_OPENGL_ADAPTERGL_H_
+#define SRC_DAWN_NATIVE_OPENGL_ADAPTERGL_H_
+
+#include "dawn/native/Adapter.h"
+#include "dawn/native/opengl/OpenGLFunctions.h"
+
+namespace dawn::native::opengl {
+
+class Adapter : public AdapterBase {
+  public:
+    Adapter(InstanceBase* instance, wgpu::BackendType backendType);
+
+    MaybeError InitializeGLFunctions(void* (*getProc)(const char*));
+
+    ~Adapter() override = default;
+
+    // AdapterBase Implementation
+    bool SupportsExternalImages() const override;
+
+  private:
+    MaybeError InitializeImpl() override;
+    MaybeError InitializeSupportedFeaturesImpl() override;
+    MaybeError InitializeSupportedLimitsImpl(CombinedLimits* limits) override;
+    ResultOrError<Ref<DeviceBase>> CreateDeviceImpl(const DeviceDescriptor* descriptor) override;
+
+    OpenGLFunctions mFunctions;
+};
+
+}  // namespace dawn::native::opengl
+
+#endif  // SRC_DAWN_NATIVE_OPENGL_ADAPTERGL_H_
