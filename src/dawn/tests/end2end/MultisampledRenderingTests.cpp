67973e67062fe2605953cea6d641e85d12aaf1ff
Brian Sheedy <bsheedy@google.com>
2022-06-10 22:12:43 +0000

    Suppress Mac Intel 12.4 failures
    Suppresses failures found when switching from 11.5.2 to 12.4
    on the Intel Mac Minis.
    
    Bug: dawn:1461, dawn:1462, dawn:1463, chromium:1334335
    
    Change-Id: Ie0d42a1b4aa81f457145a53eebb1501a5eb77fbe
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/93308
    Reviewed-by: Austin Eng <enga@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@google.com>
    Commit-Queue: Brian Sheedy <bsheedy@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>

--- src/dawn/tests/end2end/MultisampledRenderingTests.cpp
+++ src/dawn/tests/end2end/MultisampledRenderingTests.cpp
@@ -629,6 +629,9 @@ TEST_P(MultisampledRenderingTest, ResolveInto2DTextureWithEmptyFinalSampleMask) 
 // Test doing MSAA resolve into multiple resolve targets works correctly with a non-default sample
 // mask.
 TEST_P(MultisampledRenderingTest, ResolveIntoMultipleResolveTargetsWithSampleMask) {
+    // TODO(crbug.com/dawn/1462): Re-enable on Mac Intel 12.4.
+    DAWN_SUPPRESS_TEST_IF(IsMetal() && IsIntel() && IsMacOS(12, 4));
+
     wgpu::TextureView multisampledColorView2 =
         CreateTextureForRenderAttachment(kColorFormat, kSampleCount).CreateView();
     wgpu::Texture resolveTexture2 = CreateTextureForRenderAttachment(kColorFormat, 1);
@@ -785,6 +788,9 @@ TEST_P(MultisampledRenderingTest, ResolveIntoMultipleResolveTargetsWithShaderOut
     // TODO(crbug.com/dawn/673): Work around or enforce via validation that sample variables are not
     // supported on some platforms.
     DAWN_TEST_UNSUPPORTED_IF(HasToggleEnabled("disable_sample_variables"));
+
+    // TODO(crbug.com/dawn/1462): Re-enable on Mac Intel 12.4.
+    DAWN_SUPPRESS_TEST_IF(IsMetal() && IsIntel() && IsMacOS(12, 4));
 
     wgpu::TextureView multisampledColorView2 =
         CreateTextureForRenderAttachment(kColorFormat, kSampleCount).CreateView();
--- src/dawn/tests/end2end/RenderPassLoadOpTests.cpp
+++ src/dawn/tests/end2end/RenderPassLoadOpTests.cpp
@@ -246,6 +246,10 @@ TEST_P(RenderPassLoadOpTests, LoadOpClearIntegerFormatsToLargeValues) {
     // TODO(crbug.com/dawn/1109): Re-enable once fixed on Mac Mini 8,1s w/ 11.5.
     DAWN_SUPPRESS_TEST_IF(IsMetal() && IsIntel() && IsMacOS(11, 5));
 
+    // TODO(crbug.com/dawn/1463): Re-enable, might be the same as above just on
+    // 12.4 instead of 11.5.
+    DAWN_SUPPRESS_TEST_IF(IsMetal() && IsIntel() && IsMacOS(12, 4));
+
     constexpr double kUint32MaxDouble = 4294967295.0;
     constexpr uint32_t kUint32Max = static_cast<uint32_t>(kUint32MaxDouble);
     // RGBA32Uint for UINT32_MAX
