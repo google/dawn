3fa3e5667b1ca22875a9e17a0aaabab1a22d243e
Corentin Wallez <cwallez@chromium.org>
2022-06-28 15:55:16 +0000

    dawn::wire::client: ReserveTexture: remove defaulting of descriptor
    The usages in Chromium have been updated to pass a descriptor.
    
    Bug: dawn:1451
    Change-Id: I1f08141759dc7b8e0e805aa8efc9a8db51162876
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/94941
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>

--- include/dawn/wire/WireClient.h
+++ include/dawn/wire/WireClient.h
@@ -70,10 +70,8 @@ class DAWN_WIRE_EXPORT WireClient : public CommandHandler {
 
     const volatile char* HandleCommands(const volatile char* commands, size_t size) override;
 
-    // TODO(dawn:1451): Remove the defaulting of descriptor once the callers are updated to provide
-    // one.
     ReservedTexture ReserveTexture(WGPUDevice device,
-                                   const WGPUTextureDescriptor* descriptor = nullptr);
+                                   const WGPUTextureDescriptor* descriptor);
     ReservedSwapChain ReserveSwapChain(WGPUDevice device);
     ReservedDevice ReserveDevice();
     ReservedInstance ReserveInstance();
