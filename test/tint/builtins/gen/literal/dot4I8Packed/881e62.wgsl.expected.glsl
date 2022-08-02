99fa7b9e7fe567793c4fe0f1d700e779660be711
Loko Kung <lokokung@google.com>
2022-08-02 21:06:55 +0000

    test/tint: Suppresses vulkan-dep failures in crbug.com/tint/1632
    Bug: tint:1632
    Change-Id: Ie80a94821ddb0cdd467df2b9ee64adae32efde21
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/98020
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: Loko Kung <lokokung@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>

--- test/tint/builtins/gen/literal/dot4I8Packed/881e62.wgsl.expected.glsl
+++ test/tint/builtins/gen/literal/dot4I8Packed/881e62.wgsl.expected.glsl
@@ -1,1 +1,74 @@
-SKIP
+SKIP: FAILED
+
+
+enable chromium_experimental_dp4a;
+
+fn dot4I8Packed_881e62() {
+  var res : i32 = dot4I8Packed(1u, 1u);
+}
+
+@vertex
+fn vertex_main() -> @builtin(position) vec4<f32> {
+  dot4I8Packed_881e62();
+  return vec4<f32>();
+}
+
+@fragment
+fn fragment_main() {
+  dot4I8Packed_881e62();
+}
+
+@compute @workgroup_size(1)
+fn compute_main() {
+  dot4I8Packed_881e62();
+}
+
+Failed to generate: error: Unknown builtin method: dot4I8Packed
+
+enable chromium_experimental_dp4a;
+
+fn dot4I8Packed_881e62() {
+  var res : i32 = dot4I8Packed(1u, 1u);
+}
+
+@vertex
+fn vertex_main() -> @builtin(position) vec4<f32> {
+  dot4I8Packed_881e62();
+  return vec4<f32>();
+}
+
+@fragment
+fn fragment_main() {
+  dot4I8Packed_881e62();
+}
+
+@compute @workgroup_size(1)
+fn compute_main() {
+  dot4I8Packed_881e62();
+}
+
+Failed to generate: error: Unknown builtin method: dot4I8Packed
+
+enable chromium_experimental_dp4a;
+
+fn dot4I8Packed_881e62() {
+  var res : i32 = dot4I8Packed(1u, 1u);
+}
+
+@vertex
+fn vertex_main() -> @builtin(position) vec4<f32> {
+  dot4I8Packed_881e62();
+  return vec4<f32>();
+}
+
+@fragment
+fn fragment_main() {
+  dot4I8Packed_881e62();
+}
+
+@compute @workgroup_size(1)
+fn compute_main() {
+  dot4I8Packed_881e62();
+}
+
+Failed to generate: error: Unknown builtin method: dot4I8Packed
