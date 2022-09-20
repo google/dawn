19826c30c128e6003ee74c0c1aeeb2293df8d44d
Corentin Wallez <cwallez@chromium.org>
2022-09-20 14:05:42 +0000

    Update the matchers for skipped VVL messages
    The casing of some of the messages was updates in the Vulkan Validation
    Layers which made the suppression in the Vulkan backend not match
    anymore.
    
    Bug: dawn:1225
    Change-Id: I58c23986dce96453ded5b573bccee0ec90dcad91
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/103020
    Commit-Queue: Austin Eng <enga@chromium.org>
    Auto-Submit: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>

--- src/dawn/native/vulkan/BackendVk.cpp
+++ src/dawn/native/vulkan/BackendVk.cpp
@@ -73,12 +73,12 @@ constexpr SkippedMessage kSkippedMessages[] = {
     // considers the image read-only and produces a hazard. Dawn can't rely on storeOp=NONE and
     // so this is not expected to be worked around.
     // See http://crbug.com/dawn/1225 for more details.
-    {"SYNC-HAZARD-WRITE_AFTER_READ",
+    {"SYNC-HAZARD-WRITE-AFTER-READ",
      "depth aspect during store with storeOp VK_ATTACHMENT_STORE_OP_STORE. Access info (usage: "
      "SYNC_LATE_FRAGMENT_TESTS_DEPTH_STENCIL_ATTACHMENT_WRITE, prior_usage: "
      "SYNC_FRAGMENT_SHADER_SHADER_STORAGE_READ, read_barriers: VK_PIPELINE_STAGE_2_NONE"},
 
-    {"SYNC-HAZARD-WRITE_AFTER_READ",
+    {"SYNC-HAZARD-WRITE-AFTER-READ",
      "stencil aspect during store with stencilStoreOp VK_ATTACHMENT_STORE_OP_STORE. Access info "
      "(usage: SYNC_LATE_FRAGMENT_TESTS_DEPTH_STENCIL_ATTACHMENT_WRITE, prior_usage: "
      "SYNC_FRAGMENT_SHADER_SHADER_STORAGE_READ, read_barriers: VK_PIPELINE_STAGE_2_NONE"},
