232397774548edfa7b31eee325a4132c451fc945
Antonio Maiorano <amaiorano@google.com>
2022-09-27 17:30:57 +0000

    tint: add e2e test for crbug.com/tint/1557
    Bug: tint:1557
    Change-Id: I48e09af3b265443a330248afe5377b76754aea33
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/104020
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>

---
+++ test/tint/bug/tint/1557.wgsl.expected.fxc.hlsl
@@ -0,0 +1,45 @@
+SKIP: FAILED
+
+cbuffer cbuffer_u : register(b0, space0) {
+  uint4 u[1];
+};
+
+int f() {
+  return 0;
+}
+
+void g() {
+  int j = 0;
+  [loop] while (true) {
+    if ((j >= 1)) {
+      break;
+    }
+    j = (j + 1);
+    int k = f();
+  }
+}
+
+[numthreads(1, 1, 1)]
+void main() {
+  switch(asint(u[0].x)) {
+    case 0: {
+      switch(asint(u[0].x)) {
+        case 0: {
+          break;
+        }
+        default: {
+          g();
+          break;
+        }
+      }
+      break;
+    }
+    default: {
+      break;
+    }
+  }
+  return;
+}
+FXC validation failure:
+internal error: no storage type for block output
+
