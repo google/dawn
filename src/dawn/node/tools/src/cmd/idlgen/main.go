d96f7c22eda7cae816cfdf0252b94771fa487286
Ben Clayton <bclayton@google.com>
2022-10-24 22:59:44 +0000

    dawn/node: Fix missing validation errors
    Derived interfaces were not exposing their base interface's attributes / methods / constants.
    
    By fixing this, we now correctly expose the `message` property on interfaces deriving from `GPUError`.
    
    Change-Id: I2f8cb4145b589a7b148495ad36f1ae00e388a99e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/106881
    Auto-Submit: Ben Clayton <bclayton@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: Ben Clayton <bclayton@google.com>

--- src/dawn/node/tools/src/cmd/idlgen/main.go
+++ src/dawn/node/tools/src/cmd/idlgen/main.go
@@ -113,6 +113,9 @@ func run() error {
 		"EnumEntryName":              enumEntryName,
 		"Eval":                       g.eval,
 		"HasAnnotation":              hasAnnotation,
+		"FlattenedAttributesOf":      g.flattenedAttributesOf,
+		"FlattenedConstantsOf":       g.flattenedConstantsOf,
+		"FlattenedMethodsOf":         g.flattenedMethodsOf,
 		"Include":                    g.include,
 		"IsBasicLiteral":             is(ast.BasicLiteral{}),
 		"IsInitializer":              isInitializer,
@@ -571,6 +574,22 @@ func methodsOf(obj interface{}) []*Method {
 	return out
 }
 
+// flattenedMethodsOf returns all the methods of the given WebIDL
+// interface or namespace, as well as all the methods of the full inheritance
+// chain
+func (g *generator) flattenedMethodsOf(obj interface{}) []*Method {
+	switch obj := obj.(type) {
+	case *ast.Interface:
+		out := methodsOf(obj)
+		if base := g.lookup(obj.Inherits); base != nil {
+			out = append(out, g.flattenedMethodsOf(base)...)
+		}
+		return out
+	default:
+		return methodsOf(obj)
+	}
+}
+
 // attributesOf returns all the attributes of the given WebIDL interface or
 // namespace.
 func attributesOf(obj interface{}) []*ast.Member {
@@ -580,6 +599,7 @@ func attributesOf(obj interface{}) []*ast.Member {
 			out = append(out, m)
 		}
 	}
+
 	switch obj := obj.(type) {
 	case *ast.Interface:
 		for _, m := range obj.Members {
@@ -593,6 +613,22 @@ func attributesOf(obj interface{}) []*ast.Member {
 		return nil
 	}
 	return out
+}
+
+// flattenedAttributesOf returns all the attributes of the given WebIDL
+// interface or namespace, as well as all the attributes of the full inheritance
+// chain
+func (g *generator) flattenedAttributesOf(obj interface{}) []*ast.Member {
+	switch obj := obj.(type) {
+	case *ast.Interface:
+		out := attributesOf(obj)
+		if base := g.lookup(obj.Inherits); base != nil {
+			out = append(out, g.flattenedAttributesOf(base)...)
+		}
+		return out
+	default:
+		return attributesOf(obj)
+	}
 }
 
 // constantsOf returns all the constant values of the given WebIDL interface or
@@ -617,6 +653,22 @@ func constantsOf(obj interface{}) []*ast.Member {
 		return nil
 	}
 	return out
+}
+
+// flattenedConstantsOf returns all the constants of the given WebIDL
+// interface or namespace, as well as all the constants of the full inheritance
+// chain
+func (g *generator) flattenedConstantsOf(obj interface{}) []*ast.Member {
+	switch obj := obj.(type) {
+	case *ast.Interface:
+		out := constantsOf(obj)
+		if base := g.lookup(obj.Inherits); base != nil {
+			out = append(out, g.flattenedConstantsOf(base)...)
+		}
+		return out
+	default:
+		return constantsOf(obj)
+	}
 }
 
 // setlikeOf returns the setlike ast.Pattern, if obj is a setlike interface.
