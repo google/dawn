dec01f1fd5d9a35925c1af49296b9dacad1c7145
Li Hao <hao.x.li@intel.com>
2022-11-01 00:58:31 +0000

    Add timestamp period calculation for timestamp-query-inside-passes
    Timestamp period is also needed by the timestamp-query-inside-passes
    feature, which is enabled separately from the timestamp-query feature.
    
    Bug: dawn:1193
    Change-Id: I8a1f87f8d7931261b87608306820daefc4c3dc55
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/107880
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Commit-Queue: Hao Li <hao.x.li@intel.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: Austin Eng <enga@chromium.org>

--- src/dawn/native/metal/DeviceMTL.mm
+++ src/dawn/native/metal/DeviceMTL.mm
@@ -345,8 +345,10 @@ ResultOrError<ExecutionSerial> Device::CheckAndUpdateCompletedSerials() {
 MaybeError Device::TickImpl() {
     DAWN_TRY(SubmitPendingCommandBuffer());
 
-    // Just run timestamp period calculation when timestamp feature is enabled.
-    if (HasFeature(Feature::TimestampQuery)) {
+    // Just run timestamp period calculation when timestamp feature is enabled and timestamp
+    // conversion is not disabled.
+    if ((HasFeature(Feature::TimestampQuery) || HasFeature(Feature::TimestampQueryInsidePasses)) &&
+        !IsToggleEnabled(Toggle::DisableTimestampQueryConversion)) {
         if (@available(macos 10.15, iOS 14.0, *)) {
             UpdateTimestampPeriod(GetMTLDevice(), mKalmanInfo.get(), &mCpuTimestamp, &mGpuTimestamp,
                                   &mTimestampPeriod);
