279864c774bca5249194811249c2ec5fe7b1a14b
Ben Clayton <bclayton@google.com>
2022-11-04 17:49:47 +0000

    test/tint: Show that tint:1183 is fixed
    Fixed: tint:1183
    Change-Id: I06c8faf37218c58836c2a30d2b1e89abf8dc2be8
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/108643
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>

---
+++ test/tint/bug/tint/1183.wgsl.expected.glsl
@@ -0,0 +1,15 @@
+#version 310 es
+precision mediump float;
+
+layout(location = 0) out vec4 value;
+uniform highp sampler2D t_s;
+
+vec4 f() {
+  return textureOffset(t_s, vec2(0.0f), ivec2(4, 6));
+}
+
+void main() {
+  vec4 inner_result = f();
+  value = inner_result;
+  return;
+}
