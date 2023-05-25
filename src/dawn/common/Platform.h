79fe4ab42f1dd342981ef578f157862b597693fd
Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
2023-05-25 10:37:04 +0000

    [loongarch] Cleanup deprecated built-in macro.
    "_LOONGARCH_SIM" is a deprecated built-in macro, and should not be
    used anymore.
    
    Bug: chromium:1448745
    Change-Id: Ie62efd55ea5ffb8b90fca6f04feb785e5d2c1794
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/134401
    Auto-Submit: Zhao Jiazhong <zhaojiazhong-hf@loongson.cn>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>
    Kokoro: Kokoro <noreply+kokoro@google.com>

--- src/dawn/common/Platform.h
+++ src/dawn/common/Platform.h
@@ -118,10 +118,10 @@
 
 #elif defined(__loongarch__)
 #define DAWN_PLATFORM_IS_LOONGARCH 1
-#if _LOONGARCH_SIM == _ABILP32
-#define DAWN_PLATFORM_IS_LOONGARCH32 1
-#else
+#if __loongarch_grlen == 64
 #define DAWN_PLATFORM_IS_LOONGARCH64 1
+#else
+#define DAWN_PLATFORM_IS_LOONGARCH32 1
 #endif
 
 #elif defined(__riscv)
