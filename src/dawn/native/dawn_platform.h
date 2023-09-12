bec9f31c83a0d47421f308da44533698295dcca5
Jiawei Shao <jiawei.shao@intel.com>
2023-09-12 00:35:05 +0000

    Improve the readability of internal resource usages and binding types
    This patch updates the definitions of all internal resource usage bits
    and binding type bits from hexadecimal numbers to a list of "1 << n"
    expressions to improve the readability of these internal bits.
    
    Bug: dawn:1972
    Change-Id: Ia04e32da004d0f2334bedec30e94bc80640ccb28
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/150701
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Austin Eng <enga@chromium.org>

--- src/dawn/native/dawn_platform.h
+++ src/dawn/native/dawn_platform.h
@@ -32,49 +32,50 @@ constexpr uint32_t kEnumCount = EnumCount<T>::value;
 // Add an extra buffer usage and an extra binding type for binding the buffers with QueryResolve
 // usage as storage buffer in the internal pipeline.
 static constexpr wgpu::BufferUsage kInternalStorageBuffer =
-    static_cast<wgpu::BufferUsage>(0x40000000);
+    static_cast<wgpu::BufferUsage>(1u << 31);
 
 // Add an extra buffer usage (readonly storage buffer usage) for render pass resource tracking
 static constexpr wgpu::BufferUsage kReadOnlyStorageBuffer =
-    static_cast<wgpu::BufferUsage>(0x80000000);
+    static_cast<wgpu::BufferUsage>(1u << 30);
 
 static constexpr wgpu::BufferUsage kAllInternalBufferUsages =
     kInternalStorageBuffer | kReadOnlyStorageBuffer;
 
 // Extra texture usages
+// Internal usage to help tracking when a subresource is used as render attachment usage
+// more than once in a render pass.
+static constexpr wgpu::TextureUsage kAgainAsAttachment =
+    static_cast<wgpu::TextureUsage>((1u << 31) + 1);
+
 // Add an extra texture usage for textures that will be presented, for use in backends
 // that needs to transition to present usage.
 static constexpr wgpu::TextureUsage kPresentTextureUsage =
-    static_cast<wgpu::TextureUsage>(0x20000000);
+    static_cast<wgpu::TextureUsage>(1u << 30);
 
 // Add an extra texture usage (readonly render attachment usage) for render pass resource
 // tracking
 static constexpr wgpu::TextureUsage kReadOnlyRenderAttachment =
-    static_cast<wgpu::TextureUsage>(0x40000000);
+    static_cast<wgpu::TextureUsage>(1u << 29);
 
 // Add an extra texture usage (readonly storage texture usage) for resource tracking
 static constexpr wgpu::TextureUsage kReadOnlyStorageTexture =
-    static_cast<wgpu::TextureUsage>(0x08000000);
+    static_cast<wgpu::TextureUsage>(1u << 28);
 
 // Add an extra texture usage (writeonly storage texture usage) for resource tracking
 static constexpr wgpu::TextureUsage kWriteOnlyStorageTexture =
-    static_cast<wgpu::TextureUsage>(0x04000000);
-
-// Internal usage to help tracking when a subresource is used as render attachment usage
-// more than once in a render pass.
-static constexpr wgpu::TextureUsage kAgainAsAttachment =
-    static_cast<wgpu::TextureUsage>(0x80000001);
+    static_cast<wgpu::TextureUsage>(1u << 27);
 
 // Add an extra texture usage (load resolve texture to MSAA) for render pass resource tracking
 static constexpr wgpu::TextureUsage kResolveAttachmentLoadingUsage =
-    static_cast<wgpu::TextureUsage>(0x10000000);
+    static_cast<wgpu::TextureUsage>(1u << 26);
 
+// Extra BufferBindingType for internal storage buffer binding.
 static constexpr wgpu::BufferBindingType kInternalStorageBufferBinding =
-    static_cast<wgpu::BufferBindingType>(0xFFFFFFFF);
+    static_cast<wgpu::BufferBindingType>(~0u);
 
 // Extra TextureSampleType for sampling from a resolve attachment.
 static constexpr wgpu::TextureSampleType kInternalResolveAttachmentSampleType =
-    static_cast<wgpu::TextureSampleType>(0xFFFFFFFF);
+    static_cast<wgpu::TextureSampleType>(~0u);
 }  // namespace dawn::native
 
 #endif  // SRC_DAWN_NATIVE_DAWN_PLATFORM_H_
