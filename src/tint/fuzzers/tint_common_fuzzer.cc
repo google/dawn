fd8a4fd3925942ad96e1fcfe933af9452d9ef399
James Price <jrprice@google.com>
2023-09-13 17:19:09 +0000

    [fuzzers] Skip clamp_frag_depth + push constants
    The SPIR-V backend does not allow the `clamp_frag_depth` option to be
    used when the module already contains push constants.
    
    Bug: chromium:1480368
    Change-Id: I9f66ab11c74b2c50c6bd9c3b3d50e6929d387c45
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/151380
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Commit-Queue: James Price <jrprice@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>

--- src/tint/fuzzers/tint_common_fuzzer.cc
+++ src/tint/fuzzers/tint_common_fuzzer.cc
@@ -188,6 +188,16 @@ int CommonFuzzer::Run(const uint8_t* data, size_t size) {
         return 0;
     }
 
+    // Helper that returns `true` if the program uses the given extension.
+    auto uses_extension = [&program](tint::core::Extension extension) {
+        for (auto* enable : program.AST().Enables()) {
+            if (enable->HasExtension(extension)) {
+                return true;
+            }
+        }
+        return false;
+    };
+
 #if TINT_BUILD_SPV_READER
     if (input_ == InputFormat::kSpv && !SPIRVToolsValidationCheck(program, spirv_input)) {
         FATAL_ERROR(program.Diagnostics(),
@@ -289,6 +299,13 @@ int CommonFuzzer::Run(const uint8_t* data, size_t size) {
         }
         case OutputFormat::kSpv: {
 #if TINT_BUILD_SPV_WRITER
+            // Skip fuzzing the SPIR-V writer when the `clamp_frag_depth` option is used with a
+            // module that already contains push constants.
+            if (uses_extension(tint::core::Extension::kChromiumExperimentalPushConstant) &&
+                options_spirv_.clamp_frag_depth) {
+                return 0;
+            }
+
             auto result = spirv::writer::Generate(&program, options_spirv_);
             if (result) {
                 generated_spirv_ = std::move(result->spirv);
