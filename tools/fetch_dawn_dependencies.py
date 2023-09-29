7c3291bd0f70cb72e8717a6b60c785bc5112ff6e
James Price <jrprice@google.com>
2023-09-29 13:43:29 +0000

    Fetch vulkan-utility-libraries in fetch_dawn_dependencies.py
    We recently switched from vulkan-tools to vulkan-utility-libraries but
    forgot to update this script.
    
    Fixed: dawn:2100
    Change-Id: I7087df3aeb452ace1d207438a9a54a9ef4e3706b
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/154260
    Commit-Queue: James Price <jrprice@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Auto-Submit: James Price <jrprice@google.com>

--- tools/fetch_dawn_dependencies.py
+++ tools/fetch_dawn_dependencies.py
@@ -88,7 +88,7 @@ def main(args):
         'third_party/vulkan-deps/spirv-tools/src',
         'third_party/vulkan-deps/vulkan-headers/src',
         'third_party/vulkan-deps/vulkan-loader/src',
-        'third_party/vulkan-deps/vulkan-tools/src',
+        'third_party/vulkan-deps/vulkan-utility-libraries/src',
         'third_party/glfw',
         'third_party/abseil-cpp',
         'third_party/jinja2',
