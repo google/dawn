756a63627ba8a0b6e8b8e5eefa5b12d2fd43a812
Austin Eng <enga@chromium.org>
2023-10-10 23:09:48 +0000

    Remove global lock from ApiObjectBase::APIRelease
    Bug: dawn:2069
    Change-Id: Ief176254bd05ab87684f8da0d81195d645dff2b1
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/153482
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Commit-Queue: Austin Eng <enga@chromium.org>
    Reviewed-by: Loko Kung <lokokung@google.com>

--- src/dawn/common/RefCounted.h
+++ src/dawn/common/RefCounted.h
@@ -59,9 +59,7 @@ class RefCounted {
     void Release();
 
     void APIReference() { Reference(); }
-    // APIRelease() can be called without any synchronization guarantees so we need to use a Release
-    // method that will call LockAndDeleteThis() on destruction.
-    void APIRelease() { ReleaseAndLockBeforeDestroy(); }
+    void APIRelease() { Release(); }
 
   protected:
     // Friend class is needed to access the RefCount to TryIncrement.
