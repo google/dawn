6d18884a73b2803418c5510a66abd4145dd098cd
Ben Clayton <bclayton@google.com>
2023-11-15 13:44:15 +0000

    [test][tint] Fix a texel fetch test
    It used the wrong enable, resulting in a (correct) error.
    
    Change-Id: I3f7536d9a73552f12220555db94bb767e07f1dc8
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/160842
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>
    Commit-Queue: Ben Clayton <bclayton@google.com>

--- test/tint/extensions/texel_fetch/additional_params/c.wgsl.expected.glsl
+++ test/tint/extensions/texel_fetch/additional_params/c.wgsl.expected.glsl
@@ -1,6 +1,26 @@
 SKIP: FAILED
 
-extensions/texel_fetch/additional_params/c.wgsl:9:4 error: use of @color requires enabling extension 'chromium_experimental_framebuffer_fetch'
-  @color(0) fbf : vec4f,
-   ^^^^^
+
+enable chromium_experimental_framebuffer_fetch;
+
+fn g(a : f32, b : f32, c : f32) {
+}
+
+struct In {
+  @builtin(position)
+  pos : vec4f,
+  @location(0)
+  uv : vec4f,
+  @color(0)
+  fbf : vec4f,
+}
+
+@fragment
+fn f(tint_symbol : In) {
+  g(tint_symbol.pos.x, tint_symbol.uv.x, tint_symbol.fbf.y);
+}
+
+Failed to generate: extensions/texel_fetch/additional_params/c.wgsl:2:8 error: GLSL backend does not support extension 'chromium_experimental_framebuffer_fetch'
+enable chromium_experimental_framebuffer_fetch;
+       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
--- test/tint/extensions/texel_fetch/additional_params/c.wgsl.expected.wgsl
+++ test/tint/extensions/texel_fetch/additional_params/c.wgsl.expected.wgsl
@@ -1,6 +1,18 @@
-SKIP: FAILED
+enable chromium_experimental_framebuffer_fetch;
 
-extensions/texel_fetch/additional_params/c.wgsl:9:4 error: use of @color requires enabling extension 'chromium_experimental_framebuffer_fetch'
-  @color(0) fbf : vec4f,
-   ^^^^^
+fn g(a : f32, b : f32, c : f32) {
+}
 
+struct In {
+  @builtin(position)
+  pos : vec4f,
+  @location(0)
+  uv : vec4f,
+  @color(0)
+  fbf : vec4f,
+}
+
+@fragment
+fn f(in : In) {
+  g(in.pos.x, in.uv.x, in.fbf.y);
+}
