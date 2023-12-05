5ea3a8813a0dceafbe210297b028e7d4e8313ba5
dan sinclair <dsinclair@chromium.org>
2023-12-05 20:09:24 +0000

    Update test/tint/access to store results
    This CL updates the `test/tint/access` tests to make sure the results
    are stored back into a storage variable.
    
    Change-Id: If9b003ed8eb7106345962f6f93c5ae5462d0a923
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/164541
    Commit-Queue: Ben Clayton <bclayton@google.com>
    Reviewed-by: Ben Clayton <bclayton@google.com>
    Kokoro: Kokoro <noreply+kokoro@google.com>
    Auto-Submit: dan sinclair <dsinclair@chromium.org>

--- test/tint/access/var/vector.wgsl.expected.fxc.hlsl
+++ test/tint/access/var/vector.wgsl.expected.fxc.hlsl
@@ -1,8 +1,11 @@
+RWByteAddressBuffer s : register(u0);
+
 [numthreads(1, 1, 1)]
 void main() {
   float3 v = float3(0.0f, 0.0f, 0.0f);
   const float scalar = v.y;
   const float2 swizzle2 = v.xz;
   const float3 swizzle3 = v.xzy;
+  s.Store3(0u, asuint(((float3((scalar).xxx) + float3(swizzle2, 1.0f)) + swizzle3)));
   return;
 }
