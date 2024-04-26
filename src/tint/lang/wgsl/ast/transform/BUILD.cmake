f413875fd1a6f998a26b78ac20089c0ee3ee542b
Ben Clayton <bclayton@google.com>
2024-04-26 22:48:35 +0000

    [tint][fuzz][ast] Add VectorizeScalarMatrixInitializers fuzzer
    Bug: tint:2223
    Change-Id: I83d7068bcb61663dcc79083cdeced6e68453c804
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/185845
    Reviewed-by: James Price <jrprice@google.com>

--- src/tint/lang/wgsl/ast/transform/BUILD.cmake
+++ src/tint/lang/wgsl/ast/transform/BUILD.cmake
@@ -271,6 +271,7 @@ tint_add_target(tint_lang_wgsl_ast_transform_fuzz fuzz
   lang/wgsl/ast/transform/remove_unreachable_statements_fuzz.cc
   lang/wgsl/ast/transform/std140_fuzz.cc
   lang/wgsl/ast/transform/unshadow_fuzz.cc
+  lang/wgsl/ast/transform/vectorize_scalar_matrix_initializers_fuzz.cc
   lang/wgsl/ast/transform/zero_init_workgroup_memory_fuzz.cc
 )
 
