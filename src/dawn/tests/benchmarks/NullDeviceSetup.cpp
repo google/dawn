a1056518c9ada17c7fcd0eb9d42d8ed03680a0b3
James Price <jrprice@google.com>
2024-05-08 16:22:03 +0000

    Revert "(3/N) Rename WGPUDeviceLostReason_Undefined to Unknown"
    This reverts commit 98012bf2e9bdccf588624d3ba6bc045301de1658.
    
    Reason for revert: This breaks the Dawn->Chromium roll because not all usages have been updated yet. We need Emscripten to be updated with this change before we can update the remaining usages.
    
    Original change's description:
    > (3/N) Rename WGPUDeviceLostReason_Undefined to Unknown
    >
    > Spec change was:
    > https://github.com/gpuweb/gpuweb/pull/3976
    >
    > Stage 3: Remove `WGPUDeviceLostReason_Undefined`
    > from Dawn.
    >
    > Bug: chromium:42241081
    > Change-Id: I6865a9cff2494adadab4dbae898ab010401afb94
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/187280
    > Commit-Queue: Kai Ninomiya <kainino@chromium.org>
    > Reviewed-by: Austin Eng <enga@chromium.org>
    > Reviewed-by: Kai Ninomiya <kainino@chromium.org>
    
    TBR=kainino@chromium.org,enga@chromium.org,dawn-scoped@luci-project-accounts.iam.gserviceaccount.com,hocheung@chromium.org
    
    Change-Id: Iaddff04864e965a7d473a737721d52e40be72b06
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: chromium:42241081
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/187480
    Reviewed-by: James Price <jrprice@google.com>
    Reviewed-by: Austin Eng <enga@chromium.org>
    Commit-Queue: James Price <jrprice@google.com>

--- src/dawn/tests/benchmarks/NullDeviceSetup.cpp
+++ src/dawn/tests/benchmarks/NullDeviceSetup.cpp
@@ -80,7 +80,7 @@ void NullDeviceBenchmarkFixture::SetUp(const benchmark::State& state) {
 
             device.SetDeviceLostCallback(
                 [](WGPUDeviceLostReason reason, char const* message, void* userdata) {
-                    if (reason == WGPUDeviceLostReason_Unknown) {
+                    if (reason == WGPUDeviceLostReason_Undefined) {
                         dawn::ErrorLog() << message;
                         DAWN_UNREACHABLE();
                     }
