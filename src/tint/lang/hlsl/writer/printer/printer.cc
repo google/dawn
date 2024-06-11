0012e8dbb3e2d3d0f056c60cc5f5a000bc89e5d6
dan sinclair <dsinclair@chromium.org>
2024-06-11 13:31:15 +0000

    [hlsl] Emit empty functions in HLSL IR.
    This CL adds the ability to emit a function with no parameters and a
    `void` return type in the HLSL ir backend.
    
    Bug: 42251304
    Change-Id: I5a2210cc3573f105a2f2f98fee42c47a6eadcace
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/192783
    Commit-Queue: dan sinclair <dsinclair@chromium.org>
    Reviewed-by: James Price <jrprice@google.com>

--- src/tint/lang/hlsl/writer/printer/printer.cc
+++ src/tint/lang/hlsl/writer/printer/printer.cc
@@ -29,7 +29,10 @@
 
 #include <utility>
 
+#include "src/tint/lang/core/ir/module.h"
+#include "src/tint/lang/core/ir/return.h"
 #include "src/tint/lang/core/ir/validator.h"
+#include "src/tint/lang/core/type/void.h"
 #include "src/tint/utils/generator/text_generator.h"
 
 namespace tint::hlsl::writer {
@@ -49,8 +52,14 @@ class Printer : public tint::TextGenerator {
             return std::move(valid.Failure());
         }
 
-        // TODO(dsinclair): Flush out HLSL printer
+        // TOOD(dsinclair): EmitRootBlock
 
+        // Emit functions.
+        for (auto* func : ir_.DependencyOrderedFunctions()) {
+            EmitFunction(func);
+        }
+
+        result_.hlsl = main_buffer_.String();
         return std::move(result_);
     }
 
@@ -59,6 +68,71 @@ class Printer : public tint::TextGenerator {
     PrintResult result_;
 
     core::ir::Module& ir_;
+
+    /// A hashmap of value to name
+    Hashmap<const core::ir::Value*, std::string, 32> names_;
+
+    /// Emit the function
+    /// @param func the function to emit
+    void EmitFunction(const core::ir::Function* func) {
+        {
+            auto out = Line();
+            auto func_name = NameOf(func);
+
+            // TODO(dsinclair): Pipeline stage and workgroup information
+
+            EmitType(out, func->ReturnType());
+            out << " " << func_name << "(";
+
+            // TODO(dsinclair): Parameters
+
+            out << ") {";
+        }
+        {
+            const ScopedIndent si(current_buffer_);
+            EmitBlock(func->Block());
+        }
+
+        Line() << "}";
+    }
+
+    /// Emit a block
+    /// @param block the block to emit
+    void EmitBlock(const core::ir::Block* block) {
+        for (auto* inst : *block) {
+            TINT_ASSERT(inst->Is<core::ir::Return>());
+
+            // TODO(dsinclair): handle instructions
+            Line() << "return;";
+        }
+    }
+
+    /// Emit a type
+    /// @param out the stream to emit too
+    /// @param ty the type to emit
+    void EmitType(StringStream& out, const core::type::Type* ty) {
+        TINT_ASSERT(ty->Is<core::type::Void>());
+
+        // TODO(dsinclair): Emit types
+        out << "void";
+    }
+
+    /// @param value the value to get the name of
+    /// @returns the name of the given value, creating a new unique name if the value is unnamed in
+    /// the module.
+    std::string NameOf(const core::ir::Value* value) {
+        return names_.GetOrAdd(value, [&] {
+            auto sym = ir_.NameOf(value);
+            return sym.IsValid() ? sym.Name() : UniqueIdentifier("v");
+        });
+    }
+
+    /// @return a new, unique identifier with the given prefix.
+    /// @param prefix optional prefix to apply to the generated identifier. If empty
+    /// "tint_symbol" will be used.
+    std::string UniqueIdentifier(const std::string& prefix /* = "" */) {
+        return ir_.symbols.New(prefix).Name();
+    }
 };
 
 }  // namespace
