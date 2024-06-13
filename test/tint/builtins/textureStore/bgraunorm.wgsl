8f8f8d78f4ddd932a692159fed0178c0418ba9c8
Natalie Chouinard <chouinard@google.com>
2024-06-13 18:39:08 +0000

    Replace textureStore test with frag shader
    Writable textures are not allowed to be used in vertex shaders, so
    switch this non-compliant test to a fragment shader so that it doesn't fail when validation is added for this requirement in a follow up.
    
    Bug: 344846829
    Change-Id: I1314914dd1df4eabf0b6655f8d473b168c76d701
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/192980
    Commit-Queue: Natalie Chouinard <chouinard@google.com>
    Reviewed-by: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>

--- test/tint/builtins/textureStore/bgraunorm.wgsl
+++ test/tint/builtins/textureStore/bgraunorm.wgsl
@@ -1,8 +1,7 @@
 @group(0) @binding(0) var tex: texture_storage_2d<bgra8unorm, write>;
 
-@vertex
-fn vertex_main() -> @builtin(position) vec4<f32> {
+@fragment
+fn fragment_main() {
   let value = vec4(1f, 2f, 3f, 4f);
   textureStore(tex, vec2(9, 8), value);
-  return vec4<f32>();
 }
--- test/tint/builtins/textureStore/bgraunorm.wgsl.expected.fxc.hlsl
+++ test/tint/builtins/textureStore/bgraunorm.wgsl.expected.fxc.hlsl
@@ -1,18 +1,7 @@
 RWTexture2D<float4> tex : register(u0);
 
-struct tint_symbol {
-  float4 value : SV_Position;
-};
-
-float4 vertex_main_inner() {
+void fragment_main() {
   float4 value = float4(1.0f, 2.0f, 3.0f, 4.0f);
   tex[int2(9, 8)] = value;
-  return (0.0f).xxxx;
-}
-
-tint_symbol vertex_main() {
-  float4 inner_result = vertex_main_inner();
-  tint_symbol wrapper_result = (tint_symbol)0;
-  wrapper_result.value = inner_result;
-  return wrapper_result;
+  return;
 }
