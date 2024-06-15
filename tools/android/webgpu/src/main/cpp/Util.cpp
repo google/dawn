487ec21e8d5f653a93f3e73190413cfc57abba10
Jim Blackler <jimblackler@google.com>
2024-06-15 14:24:32 +0000

    Move 'Util' to a new location, and convert to Kotlin.
    It's causing difficulties having generated and handwritten files under
    the same namespace.
    
    Change-Id: I52679fb2bfb0a07c27d1fec841598c84008cf56a
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/192540
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jim Blackler <jimblackler@google.com>

--- tools/android/webgpu/src/main/cpp/Util.cpp
+++ tools/android/webgpu/src/main/cpp/Util.cpp
@@ -29,8 +29,7 @@
 
 #include <android/native_window_jni.h>
 
-extern "C" JNIEXPORT jlong JNICALL Java_android_dawn_Util_windowFromSurface(JNIEnv* env,
-                                                                            jclass thiz,
-                                                                            jobject surface) {
+extern "C" JNIEXPORT jlong JNICALL
+Java_android_dawn_helper_Util_windowFromSurface(JNIEnv* env, jclass thiz, jobject surface) {
     return reinterpret_cast<jlong>(ANativeWindow_fromSurface(env, surface));
 }
