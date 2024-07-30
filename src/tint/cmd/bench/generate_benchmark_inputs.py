fe1d09ffcb826d4093af5b9c501570effd97ed61
James Price <jrprice@google.com>
2024-07-30 21:19:58 +0000

    [tint][benchmark] Generate depfile for inputs
    This is used to make sure that we regenerate the benchmark input
    header if any of the input shaders are modified.
    
    Hardcode the list of benchmarks in the Python script instead of
    automatically using everything in the benchmark directory.
    
    Bug: 42251289
    Change-Id: I03e997043aaaef1532a4d5088fd2423e8fa22219
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/200541
    Commit-Queue: James Price <jrprice@google.com>
    Reviewed-by: dan sinclair <dsinclair@google.com>

--- src/tint/cmd/bench/generate_benchmark_inputs.py
+++ src/tint/cmd/bench/generate_benchmark_inputs.py
@@ -33,26 +33,39 @@ SPIR-V shaders, and declares macros that will be used to register them all with 
 The SPIR-V shaders are emitted as an array of uint32_t values.
 
 Usage:
-   generate_benchmark_inputs.py <header_output_path>
+   generate_benchmark_inputs.py <build_dir_path> <header_output_path>
 """
 
 import argparse
 import struct
 import sys
-from os import listdir, path
+from os import path
 
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('build_dir_path')
     parser.add_argument('header_output_path')
     args = parser.parse_args()
 
+    full_path_to_header = args.build_dir_path + '/' + args.header_output_path
+
     script_dir = path.dirname(path.realpath(__file__))
     benchmark_dir = script_dir + '/../../../../test/tint/benchmark'
-    wgsl_files = [f for f in listdir(benchmark_dir) if f.endswith('.wgsl')]
-    spv_files = [f for f in listdir(benchmark_dir) if f.endswith('.spv')]
 
-    with open(args.header_output_path, 'w') as output:
+    # The list of benchmark inputs.
+    benchmark_files = [
+        "atan2-const-eval.wgsl",
+        "cluster-lights.wgsl",
+        "metaball-isosurface.wgsl",
+        "particles.wgsl",
+        "shadow-fragment.wgsl",
+        "skinned-shadowed-pbr-fragment.wgsl",
+        "skinned-shadowed-pbr-vertex.wgsl",
+    ]
+
+    # Generate the header file.
+    with open(full_path_to_header, 'w') as output:
         print('''// AUTOMATICALLY GENERATED, DO NOT MODIFY.
 
 #ifndef SRC_TINT_CMD_BENCH_BENCHMARK_INPUTS_H_
@@ -72,29 +85,33 @@ const BenchmarkInput kBenchmarkInputs[] = {''',
               file=output)
 
         # Add an entry to the array for each benchmark.
-        for f in wgsl_files:
-            # WGSL shaders are emitted as char initializer lists.
-            with open(benchmark_dir + '/' + f, 'rb') as input:
-                print(f'    {{"{f}", {{', file=output, end='')
-                for char in input.read():
-                    print(char, file=output, end=', ')
-                print(f'}}}},', file=output)
-        for f in spv_files:
-            # SPIR-V shaders are emitted as uint32_t initializer lists.
-            with open(benchmark_dir + '/' + f, 'rb') as input:
-                print(f'    {{"{f}", nullptr, {{', file=output, end='')
-                content = input.read()
-                for word in struct.unpack("<" + ("I" * ((len(content)) // 4)),
-                                          content):
-                    print(f'{word}', file=output, end=', ')
-                print(f'}}}},', file=output)
+        for f in benchmark_files:
+            if f.endswith('.wgsl'):
+                # WGSL shaders are emitted as char initializer lists.
+                with open(benchmark_dir + '/' + f, 'rb') as input:
+                    print(f'    {{"{f}", {{', file=output, end='')
+                    for char in input.read():
+                        print(char, file=output, end=', ')
+                    print(f'}}}},', file=output)
+            elif f.endswith('.spv'):
+                # SPIR-V shaders are emitted as uint32_t initializer lists.
+                with open(benchmark_dir + '/' + f, 'rb') as input:
+                    print(f'    {{"{f}", nullptr, {{', file=output, end='')
+                    content = input.read()
+                    for word in struct.unpack(
+                            "<" + ("I" * ((len(content)) // 4)), content):
+                        print(f'{word}', file=output, end=', ')
+                    print(f'}}}},', file=output)
+            else:
+                print('unhandled file extension: ' + f)
+                return 1
 
         print('};', file=output)
         print('', file=output)
 
         # Define the macro that registers each of the inputs with Google Benchmark.
         print('#define TINT_BENCHMARK_PROGRAMS(FUNC) \\', file=output)
-        for f in sorted(wgsl_files) + sorted(spv_files):
+        for f in sorted(benchmark_files):
             print(f'    BENCHMARK_CAPTURE(FUNC, {f}, "{f}"); \\', file=output)
         print('    TINT_REQUIRE_SEMICOLON', file=output)
         print('', file=output)
@@ -104,6 +121,12 @@ const BenchmarkInput kBenchmarkInputs[] = {''',
 
 #endif  // SRC_TINT_CMD_BENCH_BENCH_H_''',
               file=output)
+
+    # Generate a depfile.
+    with open(full_path_to_header + '.d', 'w') as depfile:
+        print(args.header_output_path + ": \\", file=depfile)
+        for f in benchmark_files:
+            print("\t" + benchmark_dir + "/" + f + " \\", file=depfile)
 
 
 if __name__ == "__main__":
