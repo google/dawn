3090f89dab0459185bbd0c39c9e2a087abd9016f
James Price <jrprice@google.com>
2024-07-31 14:59:10 +0000

    [tint] Remove duplicate tint_build_wgsl_writer condition
    The top-level writer condition was clashing with the newly added
    condition for the writer benchmark. Remove the latter as it is
    unnecessary.
    
    Change-Id: Idd57f05671508a02c2b337175ddb013b65cb7ba5
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/200814
    Reviewed-by: John Stiles <johnstiles@google.com>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>
    Auto-Submit: James Price <jrprice@google.com>
    Commit-Queue: James Price <jrprice@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>

--- src/tint/lang/wgsl/writer/BUILD.cmake
+++ src/tint/lang/wgsl/writer/BUILD.cmake
@@ -145,11 +145,11 @@ if(TINT_BUILD_WGSL_WRITER)
 endif(TINT_BUILD_WGSL_WRITER)
 
 endif(TINT_BUILD_WGSL_WRITER)
-if(TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER AND TINT_BUILD_WGSL_WRITER)
+if(TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER)
 ################################################################################
 # Target:    tint_lang_wgsl_writer_bench
 # Kind:      bench
-# Condition: TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER AND TINT_BUILD_WGSL_WRITER
+# Condition: TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER
 ################################################################################
 tint_add_target(tint_lang_wgsl_writer_bench bench
   lang/wgsl/writer/writer_bench.cc
@@ -197,7 +197,7 @@ if(TINT_BUILD_WGSL_WRITER)
   )
 endif(TINT_BUILD_WGSL_WRITER)
 
-endif(TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER AND TINT_BUILD_WGSL_WRITER)
+endif(TINT_BUILD_WGSL_WRITER AND TINT_BUILD_SPV_READER AND TINT_BUILD_WGSL_READER)
 if(TINT_BUILD_WGSL_WRITER)
 ################################################################################
 # Target:    tint_lang_wgsl_writer_fuzz
