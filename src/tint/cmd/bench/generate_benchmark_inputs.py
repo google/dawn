3de551df709758f916518ce434e765de0184a4ee
James Price <jrprice@google.com>
2024-08-14 00:56:20 +0000

    [benchmark] Convert SPIR-V when generating header
    Update the benchmark header generation script to invoke Tint to
    convert SPIR-V shaders to WGSL, and have it always generate the header
    at the checked-in location.
    
    Remove the GN target (because outputs cannot be in the source
    directory), but leave the CMake target for convenience.
    
    Bug: 42251293
    Change-Id: I29baafa4890604540050fa02a023f6796f5b0b9b
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/202359
    Commit-Queue: James Price <jrprice@google.com>
    Reviewed-by: dan sinclair <dsinclair@chromium.org>

--- src/tint/cmd/bench/generate_benchmark_inputs.py
+++ src/tint/cmd/bench/generate_benchmark_inputs.py
@@ -27,31 +27,30 @@
 # OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 # OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
-Generates a header file that declares all of the Tint benchmark programs as embedded WGSL and
-SPIR-V shaders, and declares macros that will be used to register them all with Google Benchmark.
+Generates a header file that declares all of the Tint benchmark programs as embedded WGSL shaders,
+and declares macros that will be used to register them all with Google Benchmark.
 
-The SPIR-V shaders are emitted as an array of uint32_t values.
+The SPIR-V shaders are converted to WGSL using Tint before being emitted.
 
 Usage:
-   generate_benchmark_inputs.py <build_dir_path> <header_output_path>
+   generate_benchmark_inputs.py <tint>
 """
 
 import argparse
-import struct
+import subprocess
 import sys
+import tempfile
 from os import path
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('build_dir_path')
-    parser.add_argument('header_output_path')
+    parser.add_argument('tint')
     args = parser.parse_args()
-
-    full_path_to_header = args.build_dir_path + '/' + args.header_output_path
 
     script_dir = path.dirname(path.realpath(__file__))
     benchmark_dir = script_dir + '/../../../../test/tint/benchmark'
+    header_output_path = script_dir + '/inputs_bench.h'
 
     # The list of benchmark inputs.
     benchmark_files = [
@@ -65,14 +64,41 @@ def main():
     ]
 
     # Generate the header file.
-    with open(full_path_to_header, 'w') as output:
-        print('''// AUTOMATICALLY GENERATED, DO NOT MODIFY.
+    with open(header_output_path, 'w') as output:
+        print('''// Copyright 2024 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice, this
+//    list of conditions and the following disclaimer.
+//
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+//
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from
+//    this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
-#ifndef SRC_TINT_CMD_BENCH_BENCHMARK_INPUTS_H_
-#define SRC_TINT_CMD_BENCH_BENCHMARK_INPUTS_H_
+// AUTOMATICALLY GENERATED, DO NOT MODIFY DIRECTLY.
+// Regenerate using `python3 src/tint/cmd/bench/generate_benchmark_inputs.py /path/to/tint`.
 
-#include <cstdint>
-#include <vector>
+#ifndef SRC_TINT_CMD_BENCH_INPUTS_BENCH_H_
+#define SRC_TINT_CMD_BENCH_INPUTS_BENCH_H_
+
+#include <string>
 
 // clang-format off
 
@@ -81,32 +107,38 @@ namespace tint::bench {
 struct BenchmarkInput {
     const char* name = nullptr;
     const std::string wgsl;
-    const std::vector<uint32_t> spirv;
 };
 const BenchmarkInput kBenchmarkInputs[] = {''',
               file=output)
 
-        # Add an entry to the array for each benchmark.
-        for f in benchmark_files:
-            if f.endswith('.wgsl'):
-                # WGSL shaders are emitted as char initializer lists.
-                with open(benchmark_dir + '/' + f, 'rb') as input:
-                    print(f'    {{"{f}", {{', file=output, end='')
-                    for char in input.read():
-                        print(char, file=output, end=', ')
-                    print(f'}}}},', file=output)
-            elif f.endswith('.spv'):
-                # SPIR-V shaders are emitted as uint32_t initializer lists.
-                with open(benchmark_dir + '/' + f, 'rb') as input:
-                    print(f'    {{"{f}", nullptr, {{', file=output, end='')
-                    content = input.read()
-                    for word in struct.unpack(
-                            "<" + ("I" * ((len(content)) // 4)), content):
-                        print(f'{word}', file=output, end=', ')
-                    print(f'}}}},', file=output)
-            else:
-                print('unhandled file extension: ' + f)
-                return 1
+        # Helper to emit a WGSL shader as a char initializer list.
+        def emit_wgsl(input):
+            print(f'    {{"{f}", {{', file=output, end='')
+            for char in input.read():
+                print(char, file=output, end=', ')
+            print(f'}}}},', file=output)
+
+        # Create a temporary directory for converting SPIR-V shaders to WGSL.
+        with tempfile.TemporaryDirectory() as tmp:
+            # Add an entry to the array for each benchmark.
+            for f in benchmark_files:
+                if f.endswith('.wgsl'):
+                    # Emit the WGSL shader as is.
+                    with open(benchmark_dir + '/' + f, 'rb') as input:
+                        emit_wgsl(input)
+                elif f.endswith('.spv'):
+                    # Convert SPIR-V inputs to WGSL using Tint.
+                    tmpwgsl = tmp + '/tmp.wgsl'
+                    tint_args = [
+                        args.tint, '-o', tmpwgsl, '--format', 'wgsl', f,
+                        '--allow-non-uniform-derivatives'
+                    ]
+                    subprocess.run(tint_args, check=True)
+                    with open(tmpwgsl, 'rb') as input:
+                        emit_wgsl(input)
+                else:
+                    print('unhandled file extension: ' + f)
+                    return 1
 
         print('};', file=output)
         print('', file=output)
@@ -123,14 +155,8 @@ const BenchmarkInput kBenchmarkInputs[] = {''',
 
 // clang-format on
 
-#endif  // SRC_TINT_CMD_BENCH_BENCH_H_''',
+#endif  // SRC_TINT_CMD_BENCH_INPUTS_BENCH_H_''',
               file=output)
-
-    # Generate a depfile.
-    with open(full_path_to_header + '.d', 'w') as depfile:
-        print(args.header_output_path + ": \\", file=depfile)
-        for f in benchmark_files:
-            print("\t" + benchmark_dir + "/" + f + " \\", file=depfile)
 
 
 if __name__ == "__main__":
