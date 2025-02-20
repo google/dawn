9fe9d9b1bf67a3c1198a5188f5c2541f97061763
Kai Ninomiya <kainino@chromium.org>
2025-02-20 11:15:44 -0800

    Revert "fix buffer destroy"
    This reverts commit 607ea6eecee8fee7f93e439c3eab6f87e763c938.
    
    Reason for revert: Causes crashes because buffers are destroyed
    too early. We can't make this change yet because emdawnwebgpu
    doesn't have internal cross-references between objects (e.g.
    from BindGroup or CommandBuffer, to Buffer) yet like Dawn does.
    
    Original change's description:
    > fix buffer destroy
    >
    > In native implementation, wgpuBufferRelease will trigger the buffer destroy (if refcount decreased to 0). But in emwgpu implementation, the buffer destroy won't happen.
    >
    > This change fixes the bug.
    >
    > Change-Id: Ifc335b42ecab9dcde4549b377871ac3912798a41
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/226315
    > Reviewed-by: Loko Kung <lokokung@google.com>
    > Reviewed-by: Shaobo Yan <shaoboyan@microsoft.com>
    > Commit-Queue: Shaobo Yan <shaoboyan@microsoft.com>
    
    Change-Id: Id44fc5f8b7ddd41e25f7bf914a07125406ec3ab3
    No-Try: true
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/227194
    Auto-Submit: Kai Ninomiya <kainino@chromium.org>
    Reviewed-by: Loko Kung <lokokung@google.com>
    Commit-Queue: Loko Kung <lokokung@google.com>

--- third_party/emdawnwebgpu/webgpu.cpp
+++ third_party/emdawnwebgpu/webgpu.cpp
@@ -646,7 +646,6 @@ struct WGPUBufferImpl final : public EventSource,
                               public RefCountedWithExternalCount {
  public:
   WGPUBufferImpl(const EventSource* source, bool mappedAtCreation);
-  ~WGPUBufferImpl();
 
   void Destroy();
   const void* GetConstMappedRange(size_t offset, size_t size);
@@ -1287,10 +1286,6 @@ WGPUBufferImpl::WGPUBufferImpl(const EventSource* source, bool mappedAtCreation)
   if (mappedAtCreation) {
     mPendingMapRequest = {kNullFutureId, WGPUMapMode_Write};
   }
-}
-
-WGPUBufferImpl::~WGPUBufferImpl() {
-  Destroy();
 }
 
 void WGPUBufferImpl::Destroy() {
