ef26b90ad02e931fbc6a27be57f2cf985527013a
Jiawei Shao <jiawei.shao@intel.com>
2025-03-06 19:49:18 -0800

    Remove the redefinition of dawn::native::Vulkan::ClampFragDepthArgs
    This patch removes the redefinition of `ClampFragDepthArgs` in
    `PipelineLayoutVk.h` as there is another definition of it in
    `ImmediateConstantsLayout.h`.
    
    Bug: 366291600
    Change-Id: Ie309b37d049460e783c1550576de12cb99fd6a1e
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/229336
    Reviewed-by: Loko Kung <lokokung@google.com>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Commit-Queue: Jiawei Shao <jiawei.shao@intel.com>

--- src/dawn/native/ImmediateConstantsLayout.h
+++ src/dawn/native/ImmediateConstantsLayout.h
@@ -44,6 +44,7 @@ struct UserImmediateConstants {
     uint32_t userImmediateData[kMaxExternalImmediateConstantsPerPipeline];
 };
 
+// 8 bytes of push constant data to be used by the ClampFragDepth Tint transform.
 struct ClampFragDepthArgs {
     float minClampFragDepth;
     float maxClampFragDepth;
