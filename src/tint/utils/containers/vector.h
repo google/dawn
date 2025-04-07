2a39d3cea8b5bdf24855bcdb1e837c9a14799140
Ryan Harrison <rharrison@chromium.org>
2025-04-07 08:59:37 -0700

    [tint] Document unsafe buffer usage in tint::Vector
    These are fundamentally unavoidable without a complete rewrite, and
    potential performance hit, so just documenting why they are there.
    
    Bug: 394825124
    Change-Id: I171fb560bd03411db974eba4a23184e866a1a72c
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/234734
    Reviewed-by: James Price <jrprice@google.com>
    Commit-Queue: James Price <jrprice@google.com>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>
    Auto-Submit: Ryan Harrison <rharrison@chromium.org>
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>

--- src/tint/utils/containers/vector.h
+++ src/tint/utils/containers/vector.h
@@ -44,6 +44,12 @@
 #include "src/tint/utils/memory/aligned_storage.h"
 #include "src/tint/utils/memory/bitcast.h"
 
+// This file implements a custom STL style container & iterator in a performant manner, using
+// C-style data access. It is not unexpected that -Wunsafe-buffer-usage triggers in this code, since
+// the type of dynamic access being used cannot be guaranteed to be safe via static analysis.
+// Attempting to change this code in simple ways to quiet these errors either a) negatively affects
+// the performance by introducing unneeded copes, or b) uses typing shenanigans to work around the
+// warning that other linters/analyses are unhappy with.
 TINT_BEGIN_DISABLE_WARNING(UNSAFE_BUFFER_USAGE);
 
 #ifndef TINT_VECTOR_MUTATION_CHECKS_ENABLED
