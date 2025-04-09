9fd9a8f7f78462f9e34dfed477ff8fee6ef18e46
Ryan Harrison <rharrison@chromium.org>
2025-04-09 12:03:37 -0700

    [tint] Document unsafe buffer usage in tint::HashmapBase
    These are fundamentally unavoidable without a complete rewrite, and
    potential performance hit, so just documenting why they are there.
    
    Bug: 394825124
    Change-Id: I8d74ca780cbd57a5faaeebd5f1bab9bcec2e0a8c
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/235874
    Commit-Queue: Ryan Harrison <rharrison@chromium.org>
    Commit-Queue: Antonio Maiorano <amaiorano@google.com>
    Auto-Submit: Ryan Harrison <rharrison@chromium.org>
    Reviewed-by: Antonio Maiorano <amaiorano@google.com>

--- src/tint/utils/containers/hashmap_base.h
+++ src/tint/utils/containers/hashmap_base.h
@@ -41,6 +41,12 @@
 #include "src/tint/utils/memory/aligned_storage.h"
 #include "src/tint/utils/rtti/traits.h"
 
+// This file implements a custom STL style container & iterator in a performant manner, using
+// C-style data access. It is not unexpected that -Wunsafe-buffer-usage triggers in this code, since
+// the type of dynamic access being used cannot be guaranteed to be safe via static analysis.
+// Attempting to change this code in simple ways to quiet these errors either a) negatively affects
+// the performance by introducing unneeded copes, or b) uses typing shenanigans to work around the
+// warning that other linters/analyses are unhappy with.
 TINT_BEGIN_DISABLE_WARNING(UNSAFE_BUFFER_USAGE);
 
 namespace tint {
