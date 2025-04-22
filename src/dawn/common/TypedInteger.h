d2497a3e9389f0ac559d8842fb383ee204fe5852
Corentin Wallez <cwallez@chromium.org>
2025-04-22 01:56:02 -0700

    [dawn][TypedInteger] Move narrowing check as static_assert
    The use of the SFINAE in for the limit check was causing weird
    compilation failures on Build-Ubuntu24.04-Clang-x86_64-Debug-Tidywhen
    using a TypedInteger as a key of an absl::flat_map.
    
    Bug: None
    Change-Id: I3c67b28943f1363caca617f00e616fc92f660ba2
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/238394
    Reviewed-by: Fr <beaufort.francois@gmail.com>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>

--- src/dawn/common/TypedInteger.h
+++ src/dawn/common/TypedInteger.h
@@ -90,12 +90,11 @@ class alignas(T) TypedIntegerImpl {
     }
 
     // Construction from non-narrowing integral types.
-    template <typename I,
-              typename =
-                  std::enable_if_t<std::is_integral<I>::value &&
-                                   std::numeric_limits<I>::max() <= std::numeric_limits<T>::max() &&
-                                   std::numeric_limits<I>::min() >= std::numeric_limits<T>::min()>>
-    explicit constexpr TypedIntegerImpl(I rhs) : mValue(static_cast<T>(rhs)) {}
+    template <typename I, typename = std::enable_if_t<std::is_integral<I>::value>>
+    explicit constexpr TypedIntegerImpl(I rhs) : mValue(static_cast<T>(rhs)) {
+        static_assert(std::numeric_limits<I>::max() <= std::numeric_limits<T>::max());
+        static_assert(std::numeric_limits<I>::min() >= std::numeric_limits<T>::min());
+    }
 
     // Allow explicit casts only to the underlying type. If you're casting out of an
     // TypedInteger, you should know what what you're doing, and exactly what type you
