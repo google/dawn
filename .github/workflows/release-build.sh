5ef13951239685b96d358e0427fdade8457ee56c
Kai Ninomiya <kainino@chromium.org>
2025-04-22 21:11:49 -0700

    Publish emdawnwebgpu in a GitHub release
    This runs a github action which:
    - Builds emdawnwebgpu_pkg
    - Saves the version info into emdawnwebgpu_pkg/VERSION.txt
    - Zips emdawnwebgpu_pkg
    - Creates a git tag VERSION
    - Creates a release for that tag with emdawnwebgpu_pkg-VERSION.zip
    
    If someday we publish additional build artifacts, they can be published
    as additional files in the same release, alongside this one (e.g. native
    binaries, or CMake configs that automatically select between Dawn and
    emdawnwebgpu based on build target).
    
    Note the Git tags will be created via the GitHub API as they are
    required in order to publish releases on GitHub. These probably won't
    get synced into googlesource.com, but  hopefully the sync won't try to
    delete the tags. I think GitHub will prevent that, but that could cause
    the sync to stop working.
    
    Bug: 371024051
    Change-Id: I59683a8ec40e0db940e35c9e1ea8c7e678686977
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/236174
    Reviewed-by: Loko Kung <lokokung@google.com>
    Commit-Queue: Kai Ninomiya <kainino@chromium.org>

---
+++ .github/workflows/release-build.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+set -euo pipefail
+
+# Version format: vYYYYMMDD.HHMMSS or vYYYYMMDD.HHMMSS-FORKOWNER.dawn.BRANCHNAME
+VERSION_DATETIME=$(git show -s --date=format:'%Y%m%d.%H%M%S' --format=%cd)
+VERSION_SUFFIX=${GITHUB_REPOSITORY/\//.}.${GITHUB_REF_NAME}
+if [[ "$VERSION_SUFFIX" != "google.dawn.main" ]] ; then
+    PKG_VERSION=v${VERSION_DATETIME}-${VERSION_SUFFIX}
+else
+    PKG_VERSION=v${VERSION_DATETIME}
+fi
+PKG_FILE=emdawnwebgpu_pkg-${PKG_VERSION}.zip
+
+# Variables for documentation.
+SHA=$(git rev-parse HEAD)
+EMSDK_VERSION=$(python3 tools/activate-emsdk --get-emsdk-version)
+
+# Initialize dependencies
+git submodule update --init --depth=1 third_party/abseil-cpp
+git submodule update --init --depth=1 third_party/googletest/src
+git submodule update --init --depth=1 third_party/emsdk
+python3 tools/activate-emsdk
+
+# Build
+mkdir -p out/wasm
+third_party/emsdk/upstream/emscripten/emcmake cmake -S=. -B=out/wasm
+make -j4 -C out/wasm emdawnwebgpu_pkg
+
+# Create zip
+cat << EOF > out/wasm/emdawnwebgpu_pkg/VERSION.txt
+Dawn release ${PKG_VERSION} at revision <https://dawn.googlesource.com/dawn/+/${SHA}>.
+Built/tested with emsdk release ${EMSDK_VERSION}.
+EOF
+(cd out/wasm && zip -9 -r ../../${PKG_FILE} emdawnwebgpu_pkg)
+
+# Save version numbers for later steps
+if [[ -n "${GITHUB_OUTPUT:-}" ]] ; then
+    echo "PKG_VERSION=$PKG_VERSION" >> $GITHUB_OUTPUT
+    echo "PKG_FILE=$PKG_FILE" >> $GITHUB_OUTPUT
+fi
