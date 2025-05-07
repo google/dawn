22577d53dae7fb9e39dd6fe9348ccf1f926d89fe
Alan Baker <alanbaker@google.com>
2025-05-07 09:53:25 -0700

    Add duplicate VVL suppressions for updated messages
    Refs: https://crbug.com/dawn/416237596
    
    * VUIDs 00173 and 00184 changed upstream
    * Add duplicate entries to facilitate vulkan-deps roll
    
    Change-Id: I8a4fc5bb36070c2caa4993f131ef33043800ae02
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/240995
    Reviewed-by: dan sinclair <dsinclair@chromium.org>
    Auto-Submit: Alan Baker <alanbaker@google.com>
    Reviewed-by: James Price <jrprice@google.com>
    Commit-Queue: Alan Baker <alanbaker@google.com>

--- src/dawn/native/vulkan/BackendVk.cpp
+++ src/dawn/native/vulkan/BackendVk.cpp
@@ -175,10 +175,15 @@ constexpr SkippedMessage kSkippedMessages[] = {
     {"NVIDIA", "vkBindImageMemory: memoryTypeIndex"},
 
     // https://github.com/KhronosGroup/Vulkan-ValidationLayers/issues/9537
+    // https://crbug.com/dawn/416237596: remove old messages when deps roll
     {"VUID-vkCmdCopyBufferToImage-pRegions-00173",
      "Detected overlap between source and dest regions in memory"},
+    {"VUID-vkCmdCopyBufferToImage-pRegions-00173",
+     "Detected overlap between src and dst regions in memory"},
     {"VUID-vkCmdCopyImageToBuffer-pRegions-00184",
      "Detected overlap between source and dest regions in memory"},
+    {"VUID-vkCmdCopyImageToBuffer-pRegions-00184",
+     "Detected overlap between src and dst regions in memory"},
 };
 
 namespace dawn::native::vulkan {
