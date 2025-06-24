efa5461785bd7cc67d39f37e4b9550916b694e16
dan sinclair <dsinclair@chromium.org>
2025-06-24 12:42:40 -0700

    Remove unused AST transforms.
    Remove AST transforms which are no longer used.
    
    Bug: 421916945
    Change-Id: Ic62804a2eb68595f46eabcd20e280a8ceb8f0bf3
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/248355
    Reviewed-by: James Price <jrprice@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>

--- src/tint/lang/wgsl/ast/transform/BUILD.cmake
+++ src/tint/lang/wgsl/ast/transform/BUILD.cmake
@@ -39,71 +39,22 @@
 # Kind:      lib
 ################################################################################
 tint_add_target(tint_lang_wgsl_ast_transform lib
-  lang/wgsl/ast/transform/add_empty_entry_point.cc
-  lang/wgsl/ast/transform/add_empty_entry_point.h
-  lang/wgsl/ast/transform/array_length_from_uniform.cc
-  lang/wgsl/ast/transform/array_length_from_uniform.h
-  lang/wgsl/ast/transform/binding_remapper.cc
-  lang/wgsl/ast/transform/binding_remapper.h
-  lang/wgsl/ast/transform/builtin_polyfill.cc
-  lang/wgsl/ast/transform/builtin_polyfill.h
-  lang/wgsl/ast/transform/canonicalize_entry_point_io.cc
-  lang/wgsl/ast/transform/canonicalize_entry_point_io.h
   lang/wgsl/ast/transform/data.cc
   lang/wgsl/ast/transform/data.h
-  lang/wgsl/ast/transform/demote_to_helper.cc
-  lang/wgsl/ast/transform/demote_to_helper.h
-  lang/wgsl/ast/transform/direct_variable_access.cc
-  lang/wgsl/ast/transform/direct_variable_access.h
-  lang/wgsl/ast/transform/disable_uniformity_analysis.cc
-  lang/wgsl/ast/transform/disable_uniformity_analysis.h
-  lang/wgsl/ast/transform/expand_compound_assignment.cc
-  lang/wgsl/ast/transform/expand_compound_assignment.h
-  lang/wgsl/ast/transform/first_index_offset.cc
-  lang/wgsl/ast/transform/first_index_offset.h
-  lang/wgsl/ast/transform/fold_constants.cc
-  lang/wgsl/ast/transform/fold_constants.h
-  lang/wgsl/ast/transform/get_insertion_point.cc
-  lang/wgsl/ast/transform/get_insertion_point.h
-  lang/wgsl/ast/transform/hoist_to_decl_before.cc
-  lang/wgsl/ast/transform/hoist_to_decl_before.h
   lang/wgsl/ast/transform/manager.cc
   lang/wgsl/ast/transform/manager.h
-  lang/wgsl/ast/transform/multiplanar_external_texture.cc
-  lang/wgsl/ast/transform/multiplanar_external_texture.h
-  lang/wgsl/ast/transform/promote_initializers_to_let.cc
-  lang/wgsl/ast/transform/promote_initializers_to_let.h
-  lang/wgsl/ast/transform/promote_side_effects_to_decl.cc
-  lang/wgsl/ast/transform/promote_side_effects_to_decl.h
-  lang/wgsl/ast/transform/remove_continue_in_switch.cc
-  lang/wgsl/ast/transform/remove_continue_in_switch.h
-  lang/wgsl/ast/transform/remove_phonies.cc
-  lang/wgsl/ast/transform/remove_phonies.h
-  lang/wgsl/ast/transform/renamer.cc
-  lang/wgsl/ast/transform/renamer.h
-  lang/wgsl/ast/transform/robustness.cc
-  lang/wgsl/ast/transform/robustness.h
   lang/wgsl/ast/transform/simplify_pointers.cc
   lang/wgsl/ast/transform/simplify_pointers.h
-  lang/wgsl/ast/transform/single_entry_point.cc
-  lang/wgsl/ast/transform/single_entry_point.h
-  lang/wgsl/ast/transform/substitute_override.cc
-  lang/wgsl/ast/transform/substitute_override.h
   lang/wgsl/ast/transform/transform.cc
   lang/wgsl/ast/transform/transform.h
   lang/wgsl/ast/transform/unshadow.cc
   lang/wgsl/ast/transform/unshadow.h
-  lang/wgsl/ast/transform/vectorize_scalar_matrix_initializers.cc
-  lang/wgsl/ast/transform/vectorize_scalar_matrix_initializers.h
-  lang/wgsl/ast/transform/zero_init_workgroup_memory.cc
-  lang/wgsl/ast/transform/zero_init_workgroup_memory.h
 )
 
 tint_target_add_dependencies(tint_lang_wgsl_ast_transform lib
   tint_api_common
   tint_lang_core
   tint_lang_core_constant
-  tint_lang_core_ir_transform
   tint_lang_core_type
   tint_lang_wgsl
   tint_lang_wgsl_ast
@@ -133,35 +84,11 @@ if(TINT_BUILD_WGSL_READER AND TINT_BUILD_WGSL_WRITER)
 # Condition: TINT_BUILD_WGSL_READER AND TINT_BUILD_WGSL_WRITER
 ################################################################################
 tint_add_target(tint_lang_wgsl_ast_transform_test test
-  lang/wgsl/ast/transform/add_empty_entry_point_test.cc
-  lang/wgsl/ast/transform/array_length_from_uniform_test.cc
-  lang/wgsl/ast/transform/binding_remapper_test.cc
-  lang/wgsl/ast/transform/builtin_polyfill_test.cc
-  lang/wgsl/ast/transform/canonicalize_entry_point_io_test.cc
-  lang/wgsl/ast/transform/demote_to_helper_test.cc
-  lang/wgsl/ast/transform/direct_variable_access_test.cc
-  lang/wgsl/ast/transform/disable_uniformity_analysis_test.cc
-  lang/wgsl/ast/transform/expand_compound_assignment_test.cc
-  lang/wgsl/ast/transform/first_index_offset_test.cc
-  lang/wgsl/ast/transform/fold_constants_test.cc
-  lang/wgsl/ast/transform/get_insertion_point_test.cc
   lang/wgsl/ast/transform/helper_test.h
-  lang/wgsl/ast/transform/hoist_to_decl_before_test.cc
   lang/wgsl/ast/transform/manager_test.cc
-  lang/wgsl/ast/transform/multiplanar_external_texture_test.cc
-  lang/wgsl/ast/transform/promote_initializers_to_let_test.cc
-  lang/wgsl/ast/transform/promote_side_effects_to_decl_test.cc
-  lang/wgsl/ast/transform/remove_continue_in_switch_test.cc
-  lang/wgsl/ast/transform/remove_phonies_test.cc
-  lang/wgsl/ast/transform/renamer_test.cc
-  lang/wgsl/ast/transform/robustness_test.cc
   lang/wgsl/ast/transform/simplify_pointers_test.cc
-  lang/wgsl/ast/transform/single_entry_point_test.cc
-  lang/wgsl/ast/transform/substitute_override_test.cc
   lang/wgsl/ast/transform/transform_test.cc
   lang/wgsl/ast/transform/unshadow_test.cc
-  lang/wgsl/ast/transform/vectorize_scalar_matrix_initializers_test.cc
-  lang/wgsl/ast/transform/zero_init_workgroup_memory_test.cc
 )
 
 tint_target_add_dependencies(tint_lang_wgsl_ast_transform_test test
@@ -169,7 +96,6 @@ tint_target_add_dependencies(tint_lang_wgsl_ast_transform_test test
   tint_lang_core
   tint_lang_core_constant
   tint_lang_core_ir
-  tint_lang_core_ir_transform
   tint_lang_core_type
   tint_lang_wgsl
   tint_lang_wgsl_ast
