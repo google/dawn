2e5296af62b62fee638a3d5ff5f0ae95e683d774
dan sinclair <dsinclair@chromium.org>
2025-07-15 16:33:33 -0700

    [ir-to-program] Fix issue converting int32_t min.
    When converting an `i32` minimum value from IR back to WGSL, we can't
    just emit the number as it won't parse in WGSL. We need to emit it as an
    abstract and convert that to an i32 value.
    
    Change-Id: I173d1a78f78c8af8ab9791c8b72f879b67520276
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/252954
    Commit-Queue: David Neto <dneto@google.com>
    Reviewed-by: David Neto <dneto@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>

--- src/tint/lang/wgsl/writer/ir_to_program/ir_to_program.cc
+++ src/tint/lang/wgsl/writer/ir_to_program/ir_to_program.cc
@@ -27,6 +27,7 @@
 
 #include "src/tint/lang/wgsl/writer/ir_to_program/ir_to_program.h"
 
+#include <limits>
 #include <string>
 #include <tuple>
 #include <utility>
@@ -962,7 +963,15 @@ class State {
         };
         return tint::Switch(
             c->Type(),  //
-            [&](const core::type::I32*) { return b.Expr(c->ValueAs<i32>()); },
+            [&](const core::type::I32*) -> const ast::Expression* {
+                auto val = c->ValueAs<i32>();
+                if (val == std::numeric_limits<int32_t>::min()) {
+                    return b.Call(b.ty.i32(), b.create<ast::IntLiteralExpression>(
+                                                  val, ast::IntLiteralExpression::Suffix::kNone));
+                }
+
+                return b.Expr(val);
+            },
             [&](const core::type::U32*) { return b.Expr(c->ValueAs<u32>()); },
             [&](const core::type::F32*) { return b.Expr(c->ValueAs<f32>()); },
             [&](const core::type::F16*) {
