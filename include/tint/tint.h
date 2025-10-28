b8b71ece0e8d651e396f2328bd29920c1147b490
dan sinclair <dsinclair@chromium.org>
2025-10-28 09:50:52 -0700

    Remove SingleEntryPoint from public header.
    Removes the SingleEntryPoint transform inclusion from tint.h as it is
    always run by the backends.
    
    Change-Id: If5b04e1b327940c4ec1aeb081419b44c0d775b28
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/268256
    Reviewed-by: James Price <jrprice@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>

--- include/tint/tint.h
+++ include/tint/tint.h
@@ -37,7 +37,6 @@
 #include "src/tint/api/common/substitute_overrides_config.h"
 #include "src/tint/api/common/vertex_pulling_config.h"
 #include "src/tint/api/tint.h"
-#include "src/tint/lang/core/ir/transform/single_entry_point.h"
 #include "src/tint/lang/core/type/manager.h"
 #include "src/tint/lang/wgsl/inspector/inspector.h"
 #include "src/tint/utils/diagnostic/formatter.h"
