f8e6bf12edeac3d539541f8a238cca2e1b3419ef
Jim Blackler <jimblackler@google.com>
2025-11-12 02:02:48 -0800

    Kotlin: Fix API lint error in the exceptions class
    Supply a @file:JvmName attribute for Java use.
    
    Bug: b/459782075
    Change-Id: Id4697980b367e94b125ea644eac25ab36c8417af
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/272814
    Reviewed-by: Mridul Goyal <mridulgoyal@google.com>
    Commit-Queue: Jim Blackler <jimblackler@google.com>

--- generator/templates/art/api_kotlin_exceptions.kt
+++ generator/templates/art/api_kotlin_exceptions.kt
@@ -24,6 +24,8 @@
 //* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+@file:JvmName("ExceptionsKt")
+
 package {{ kotlin_package }}
 {% set ns = namespace() %}
 {% for enum in by_category["enum"] %}
