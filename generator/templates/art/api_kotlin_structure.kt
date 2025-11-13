7fac4de792f5b59b745f2c56c2ca12ec6e0b81e0
mridulgoyal <mridulgoyal@google.com>
2025-11-13 04:24:14 -0800

    Kotlin: Update the copyright year from `2024` to `2025`
    BUG: b/460206115
    Change-Id: I833a696b480ca10e02ace164cd90cdc2d6d2c71d
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/273455
    Commit-Queue: Mridul Goyal <mridulgoyal@google.com>
    Reviewed-by: Jim Blackler <jimblackler@google.com>

--- generator/templates/art/api_kotlin_structure.kt
+++ generator/templates/art/api_kotlin_structure.kt
@@ -1,4 +1,4 @@
-// Copyright 2024 The Dawn & Tint Authors
+// Copyright 2025 The Dawn & Tint Authors
 //
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
