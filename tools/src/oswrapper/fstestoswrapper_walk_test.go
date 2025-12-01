1103be27b6b84548f6a9632b913fdd07a2145716
Brian Sheedy <bsheedy@google.com>
2025-12-01 13:57:20 -0800

    Separate out FSTestOSWrapper Walk tests
    Moves tests for Walk() in FSTestOSWrapper into
    fstestoswrapper_walk_test.go. This is part of making the test file
    less monolithic, both for better maintainability and (hopefully)
    better performance with LLM-powered tools.
    
    Bug: 436025865
    Change-Id: I3840e9eac27f1cd17393069f909c8e3b8213befb
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/276823
    Reviewed-by: Ryan Harrison <rharrison@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@google.com>

---
+++ tools/src/oswrapper/fstestoswrapper_walk_test.go
@@ -0,0 +1,304 @@
+// Copyright 2025 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+package oswrapper_test
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Tests for the Walk() function in FSTestOSWrapper.
+
+// NOTE: There are two types of tests in this file, those suffixed with _MatchesReal and those that
+// are not. Those that are suffixed are meant to be testing the behaviour of the FSTestOSWrapper
+// against the RealOSWrapper to confirm that it is a drop in replacement. Those that are not
+// suffixed are traditional unittests that test the implementation functions in isolation against
+// defined expectations.
+
+func TestFSTestOSWrapper_Walk(t *testing.T) {
+	root := getTestRoot()
+	tests := []struct {
+		name          string
+		setup         unittestSetup
+		root          string
+		walkFn        func(t *testing.T, visited *[]string) filepath.WalkFunc
+		expectedPaths []string
+		expectedError
+	}{
+		{
+			name: "Walk directory structure",
+			root: root,
+			setup: unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join(root, "dir", "file2.txt"):          "",
+					filepath.Join(root, "dir", "subdir", "file.txt"): "",
+				},
+				initialDirs: []string{filepath.Join(root, "dir", "subdir")},
+			},
+			walkFn: func(t *testing.T, visited *[]string) filepath.WalkFunc {
+				return func(path string, info os.FileInfo, err error) error {
+					require.NoError(t, err)
+					*visited = append(*visited, path)
+					return nil
+				}
+			},
+			expectedPaths: []string{
+				root,
+				filepath.Join(root, "dir"),
+				filepath.Join(root, "dir", "file2.txt"),
+				filepath.Join(root, "dir", "subdir"),
+				filepath.Join(root, "dir", "subdir", "file.txt"),
+			},
+		},
+		{
+			name: "Walk non-existent root",
+			root: filepath.Join(root, "nonexistent"),
+			walkFn: func(t *testing.T, visited *[]string) filepath.WalkFunc {
+				return func(path string, info os.FileInfo, err error) error {
+					require.Error(t, err, "expected an error for non-existent root")
+					require.ErrorIs(t, err, os.ErrNotExist)
+					require.Nil(t, info, "info should be nil on error")
+					require.Equal(t, filepath.Join(root, "nonexistent"), path)
+					*visited = append(*visited, path)
+					return err // Propagate the error to stop the walk and return it from Walk()
+				}
+			},
+			expectedPaths: []string{filepath.Join(root, "nonexistent")},
+			expectedError: expectedError{
+				wantErrIs: os.ErrNotExist,
+			},
+		},
+		{
+			name: "Walk a file",
+			root: filepath.Join(root, "file.txt"),
+			setup: unittestSetup{
+				initialFiles: map[string]string{filepath.Join(root, "file.txt"): ""},
+			},
+			walkFn: func(t *testing.T, visited *[]string) filepath.WalkFunc {
+				return func(path string, info os.FileInfo, err error) error {
+					require.NoError(t, err)
+					*visited = append(*visited, path)
+					return nil
+				}
+			},
+			expectedPaths: []string{filepath.Join(root, "file.txt")},
+		},
+		{
+			name: "Error from walk function",
+			root: root,
+			setup: unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join(root, "dir", "a"): "",
+					filepath.Join(root, "dir", "b"): "",
+				},
+				initialDirs: []string{filepath.Join(root, "dir")},
+			},
+			walkFn: func(t *testing.T, visited *[]string) filepath.WalkFunc {
+				return func(path string, info os.FileInfo, err error) error {
+					if strings.HasSuffix(path, "b") {
+						return fmt.Errorf("stop walking")
+					}
+					return nil
+				}
+			},
+			expectedError: expectedError{
+				wantErrMsg: "stop walking",
+			},
+		},
+		{
+			name: "Skip a directory",
+			root: root,
+			setup: unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join(root, "dir", "subdir", "file.txt"):      "",
+					filepath.Join(root, "dir", "anotherdir", "file2.txt"): "",
+				},
+				initialDirs: []string{
+					filepath.Join(root, "dir", "subdir"),
+					filepath.Join(root, "dir", "anotherdir"),
+				},
+			},
+			walkFn: func(t *testing.T, visited *[]string) filepath.WalkFunc {
+				return func(path string, info os.FileInfo, err error) error {
+					require.NoError(t, err)
+					*visited = append(*visited, path)
+					if info.IsDir() && path == filepath.Join(root, "dir", "subdir") {
+						return filepath.SkipDir
+					}
+					return nil
+				}
+			},
+			expectedPaths: []string{
+				root,
+				filepath.Join(root, "dir"),
+				filepath.Join(root, "dir", "anotherdir"),
+				filepath.Join(root, "dir", "anotherdir", "file2.txt"),
+				filepath.Join(root, "dir", "subdir"),
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			wrapper := tc.setup.setup(t)
+
+			var visited []string
+			var walkFn filepath.WalkFunc
+			if tc.walkFn != nil {
+				walkFn = tc.walkFn(t, &visited)
+			}
+
+			err := wrapper.Walk(tc.root, walkFn)
+
+			if !tc.expectedError.Check(t, err) {
+				if tc.expectedPaths != nil {
+					require.ElementsMatch(t, tc.expectedPaths, visited)
+				}
+			}
+		})
+	}
+}
+
+func TestFSTestOSWrapper_Walk_MatchesReal(t *testing.T) {
+	tests := []struct {
+		name         string
+		setup        matchesRealSetup
+		root         string
+		walkBehavior func(path string, info os.FileInfo) error
+	}{
+		{
+			name: "Simple walk",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join("dir", "subdir", "file.txt"): "",
+					filepath.Join("dir", "file2.txt"):          "",
+				},
+				initialDirs: []string{
+					"dir",
+					filepath.Join("dir", "subdir"),
+				},
+			}},
+			root: "dir",
+		},
+		{
+			name: "Walk a file",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{"file.txt": ""},
+			}},
+			root: "file.txt",
+		},
+		{
+			name: "Walk non-existent root",
+			root: "nonexistent",
+		},
+		{
+			name: "Skip a directory",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join("dir", "subdir", "file.txt"):     "",
+					filepath.Join("dir", "anotherdir", "file.txt"): "",
+				},
+				initialDirs: []string{
+					"dir",
+					filepath.Join("dir", "subdir"),
+					filepath.Join("dir", "anotherdir"),
+				},
+			}},
+			root: "dir",
+			walkBehavior: func(path string, info os.FileInfo) error {
+				if info.IsDir() && strings.HasSuffix(path, "subdir") {
+					return filepath.SkipDir
+				}
+				return nil
+			},
+		},
+		{
+			name: "Error from walk function",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join("dir", "a"): "",
+					filepath.Join("dir", "b"): "",
+				},
+				initialDirs: []string{"dir"},
+			}},
+			root: "dir",
+			walkBehavior: func(path string, info os.FileInfo) error {
+				if !info.IsDir() && strings.HasSuffix(path, "b") {
+					return errors.New("stop walking")
+				}
+				return nil
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			realRoot, realFS, testFS := tc.setup.setup(t)
+			defer os.RemoveAll(realRoot)
+
+			// Execute and Compare
+			var realPaths []string
+			realWalkFn := func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				relPath, err := filepath.Rel(realRoot, path)
+				require.NoError(t, err)
+				realPaths = append(realPaths, filepath.ToSlash(relPath))
+				if tc.walkBehavior != nil {
+					return tc.walkBehavior(relPath, info)
+				}
+				return nil
+			}
+			realErr := realFS.Walk(filepath.Join(realRoot, tc.root), realWalkFn)
+
+			var testPaths []string
+			testWalkFn := func(path string, info os.FileInfo, err error) error {
+				if err != nil {
+					return err
+				}
+				relPath := strings.TrimPrefix(path, "/")
+				testPaths = append(testPaths, relPath)
+				if tc.walkBehavior != nil {
+					return tc.walkBehavior(relPath, info)
+				}
+				return nil
+			}
+			testErr := testFS.Walk(tc.root, testWalkFn)
+
+			requireErrorsMatch(t, realErr, testErr)
+
+			require.ElementsMatch(t, realPaths, testPaths)
+		})
+	}
+}
