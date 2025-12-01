35e5fee270cbda0c31d0a8b69244f6f1c5025ee8
Brian Sheedy <bsheedy@google.com>
2025-12-01 13:57:20 -0800

    Separate out FSTestOSWrapper Remove tests
    Moves tests for Remove() in FSTestOSWrapper into
    fstestoswrapper_remove_test.go. This is part of making the test file
    less monolithic, both for better maintainability and (hopefully)
    better performance with LLM-powered tools.
    
    Bug: 436025865
    Change-Id: Id093ff9edd9b20da0e8bb35e6823790355e0b935
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/276828
    Reviewed-by: Ryan Harrison <rharrison@chromium.org>
    Commit-Queue: Brian Sheedy <bsheedy@google.com>

---
+++ tools/src/oswrapper/fstestoswrapper_remove_test.go
@@ -0,0 +1,202 @@
+// Copyright 2025 The Dawn & Tint Authors
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//  1. Redistributions of source code must retain the above copyright notice, this
+//     list of conditions and the following disclaimer.
+//
+//  2. Redistributions in binary form must reproduce the above copyright notice,
+//     this list of conditions and the following disclaimer in the documentation
+//     and/or other materials provided with the distribution.
+//
+//  3. Neither the name of the copyright holder nor the names of its
+//     contributors may be used to endorse or promote products derived from
+//     this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+// DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+// FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+// DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+// SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+// CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+// OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+// OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+package oswrapper_test
+
+import (
+	"os"
+	"path/filepath"
+	"syscall"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// Tests for the Remove() function in FSTestOSWrapper.
+
+// NOTE: There are two types of tests in this file, those suffixed with _MatchesReal and those that
+// are not. Those that are suffixed are meant to be testing the behaviour of the FSTestOSWrapper
+// against the RealOSWrapper to confirm that it is a drop in replacement. Those that are not
+// suffixed are traditional unittests that test the implementation functions in isolation against
+// defined expectations.
+
+func TestFSTestOSWrapper_Remove(t *testing.T) {
+	root := getTestRoot()
+	tests := []struct {
+		name          string
+		setup         unittestSetup
+		path          string
+		expectMissing []string
+		expectPresent []string
+		expectedError
+	}{
+		{
+			name: "Remove file",
+			path: filepath.Join(root, "file.txt"),
+			setup: unittestSetup{
+				initialFiles: map[string]string{filepath.Join(root, "file.txt"): "content"},
+			},
+			expectMissing: []string{filepath.Join(root, "file.txt")},
+		},
+		{
+			name: "Remove empty directory",
+			path: filepath.Join(root, "emptydir"),
+			setup: unittestSetup{
+				initialDirs: []string{filepath.Join(root, "emptydir")},
+			},
+			expectMissing: []string{filepath.Join(root, "emptydir")},
+		},
+		{
+			name: "Remove file, others retained",
+			path: filepath.Join(root, "file.txt"),
+			setup: unittestSetup{
+				initialFiles: map[string]string{
+					filepath.Join(root, "file.txt"):     "content",
+					filepath.Join(root, "other.txt"):    "other content",
+					filepath.Join(root, "dir", "f.txt"): "in dir",
+				},
+				initialDirs: []string{filepath.Join(root, "otherdir")},
+			},
+			expectMissing: []string{filepath.Join(root, "file.txt")},
+			expectPresent: []string{
+				filepath.Join(root, "other.txt"),
+				filepath.Join(root, "otherdir"),
+				filepath.Join(root, "dir"),
+				filepath.Join(root, "dir", "f.txt"),
+			},
+		},
+		{
+			name: "Error on non-existent path",
+			path: filepath.Join(root, "nonexistent"),
+			expectedError: expectedError{
+				wantErrIs: os.ErrNotExist,
+			},
+		},
+		{
+			name: "Error on non-empty directory",
+			path: filepath.Join(root, "nonemptydir"),
+			setup: unittestSetup{
+				initialFiles: map[string]string{filepath.Join(root, "nonemptydir", "file.txt"): "content"},
+				initialDirs:  []string{filepath.Join(root, "nonemptydir")},
+			},
+			expectPresent: []string{
+				filepath.Join(root, "nonemptydir"),
+				filepath.Join(root, "nonemptydir", "file.txt"),
+			},
+			expectedError: expectedError{
+				wantErrIs: syscall.ENOTEMPTY,
+			},
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			wrapper := tc.setup.setup(t)
+			err := wrapper.Remove(tc.path)
+
+			for _, p := range tc.expectPresent {
+				_, statErr := wrapper.Stat(p)
+				require.NoError(t, statErr, "path '%s' should exist but it does not", p)
+			}
+
+			if tc.expectedError.Check(t, err) {
+				return
+			}
+
+			for _, p := range tc.expectMissing {
+				_, statErr := wrapper.Stat(p)
+				require.Error(t, statErr, "path '%s' should not exist but it does", p)
+				require.True(t, os.IsNotExist(statErr))
+			}
+		})
+	}
+}
+
+func TestFSTestOSWrapper_Remove_MatchesReal(t *testing.T) {
+	tests := []struct {
+		name         string
+		setup        matchesRealSetup
+		pathToRemove string
+	}{
+		{
+			name: "Remove file",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{"file.txt": "content"},
+			}},
+			pathToRemove: "file.txt",
+		},
+		{
+			name: "Remove file, others retained",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{
+					"file.txt":                    "content",
+					"other.txt":                   "other content",
+					filepath.Join("dir", "f.txt"): "in dir",
+				},
+				initialDirs: []string{
+					"dir",
+					"otherdir",
+				},
+			}},
+			pathToRemove: "file.txt",
+		},
+		{
+			name: "Remove empty directory",
+			setup: matchesRealSetup{unittestSetup{
+				initialDirs: []string{"emptydir"},
+			}},
+			pathToRemove: "emptydir",
+		},
+		{
+			name:         "Error on non-existent path",
+			pathToRemove: "nonexistent",
+		},
+		{
+			name: "Error on non-empty directory",
+			setup: matchesRealSetup{unittestSetup{
+				initialFiles: map[string]string{filepath.Join("dir", "file.txt"): "content"},
+				initialDirs:  []string{"dir"},
+			}},
+			pathToRemove: "dir",
+		},
+	}
+
+	for _, tc := range tests {
+		t.Run(tc.name, func(t *testing.T) {
+			realRoot, realFS, testFS := tc.setup.setup(t)
+			defer os.RemoveAll(realRoot)
+
+			// Execute
+			realErr := realFS.Remove(filepath.Join(realRoot, tc.pathToRemove))
+			testErr := testFS.Remove(tc.pathToRemove)
+
+			requireErrorsMatch(t, realErr, testErr)
+			if realErr == nil {
+				requireFileSystemsMatch(t, realRoot, testFS)
+			}
+		})
+	}
+}
