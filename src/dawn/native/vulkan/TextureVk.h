61305a63931c0475f21a6b54940c69cffc57de80
Brandon Jones <bajones@chromium.org>
2025-12-03 11:55:06 -0800

    Vulkan: Add support for RenderDoc process injection captures
    Updates and extends the D3D12 RenderDoc supported added by amaiorano@
    to the Vulkan backend.
    
    Also abstract the dynamic library loading necessary to a utils/ file and
    cleans up the build files a bit to reduce code duplication.
    
    Bug: 449708316
    Change-Id: I1985737eff8b1af2bc0a929e4035ae93bf22d6fb
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/276537
    Commit-Queue: Brandon Jones <bajones@chromium.org>
    Reviewed-by: Kai Ninomiya <kainino@chromium.org>

--- src/dawn/native/vulkan/TextureVk.h
+++ src/dawn/native/vulkan/TextureVk.h
@@ -109,6 +109,10 @@ class Texture : public TextureBase {
 
     void SetLabelHelper(const char* prefix);
 
+    void NotifySwapChainPresent();
+
+    void SetIsExternalSwapchainTexture(bool isSwapChainTexture);
+
     // Dawn API
     void SetLabelImpl() override;
 
@@ -166,6 +170,8 @@ class Texture : public TextureBase {
 
     SubresourceStorage<TextureSyncInfo> mSubresourceLastSyncInfos;
     VkImage mHandle = VK_NULL_HANDLE;
+
+    bool mIsExternalSwapChainTexture = false;
 };
 
 // A texture created and fully owned by Dawn. Typically the result of device.CreateTexture.
