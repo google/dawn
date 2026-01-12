224f2b35214f236bd2d67a435011061d48fcf451
Mridul Goyal <mridulgoyal@google.com>
2026-01-12 03:14:43 -0800

    Kotlin: Removed extra line space from generated .kt files
    Change-Id: Ifc95c90784936218e89b6af8f543713204c85ff9
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/284279
    Commit-Queue: Mridul Goyal <mridulgoyal@google.com>
    Reviewed-by: Tarun Saini <sainitarun@google.com>

--- generator/templates/art/api_kotlin_async_helpers.kt
+++ generator/templates/art/api_kotlin_async_helpers.kt
@@ -24,7 +24,6 @@
 //* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 package {{ kotlin_package }}
 
 {% from 'art/api_kotlin_types.kt' import kotlin_annotation, kotlin_declaration, kotlin_definition, check_if_doc_present, generate_kdoc with context %}
--- generator/templates/art/api_kotlin_structure.kt
+++ generator/templates/art/api_kotlin_structure.kt
@@ -24,7 +24,6 @@
 //* CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
 //* OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
 //* OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-
 package {{ kotlin_package }}
 {% from 'art/api_kotlin_types.kt' import kotlin_annotation, kotlin_definition, generate_simple_kdoc with context %}
 
