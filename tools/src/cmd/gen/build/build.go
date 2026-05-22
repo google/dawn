f924e1a8a588f1459bb7d9574f7a6540ff194f5f
James Price <jrprice@google.com>
2026-05-22 06:40:44 -0700

    Revert "[gn] Add support for generated sources"
    This reverts commit ad0da6af0764fbdfd6a673e3ce14c27ac948e6b0.
    
    Reason for revert: break iOS builds in Chromium due to failure to find Go executable
    
    Original change's description:
    > [gn] Add support for generated sources
    >
    > Add a custom action that invokes the source generator. The build file
    > generator produces a list of dependencies for this custom command, so
    > that source generation will automatically re-run whenever any .tmpl
    > file or .def file is changed.
    >
    > For GN we also pre-generate a list of generated sources because it
    > needs to know what the outputs of the command are. Refactor some of
    > the common code for generating these lists into helper functions.
    >
    > Bug: 513255285
    > Change-Id: I8bd53a4073ecc099b89b5c7f584278936dcbf2f1
    > Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/309180
    > Reviewed-by: dan sinclair <dsinclair@chromium.org>
    > Commit-Queue: James Price <jrprice@google.com>
    
    TBR=dsinclair@chromium.org,jrprice@google.com,dawn-scoped@luci-project-accounts.iam.gserviceaccount.com
    
    No-Presubmit: true
    No-Tree-Checks: true
    No-Try: true
    Bug: 513255285
    Change-Id: I59e7dd68df16740d82dbbc31d965b74491cf6856
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/310475
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>
    Reviewed-by: James Price <jrprice@google.com>
    Reviewed-by: Alan Baker <alanbaker@google.com>
    Commit-Queue: James Price <jrprice@google.com>

--- tools/src/cmd/gen/build/build.go
+++ tools/src/cmd/gen/build/build.go
@@ -794,9 +794,6 @@ func emitBuildFiles(p *Project, fsReaderWriter oswrapper.FilesystemReaderWriter)
 	if err := emitGeneratedSourceListForCMake(p, fsReaderWriter, generatedSources, depsForGeneratedSources); err != nil {
 		return err
 	}
-	if err := emitGeneratedSourceListForGN(p, fsReaderWriter, generatedSources, depsForGeneratedSources); err != nil {
-		return err
-	}
 
 	return nil
 }
@@ -850,19 +847,6 @@ func emitGeneratedSourceListForCMake(p *Project, fsReaderWriter oswrapper.Filesy
 	emitList(sb, depsForGeneratedSources, "set(TINT_GENERATED_SOURCE_DEPS\n", ")\n", "  \"", "\"\n")
 
 	return writeFileIfStale(p, fsReaderWriter, depsPath, sb.String())
-}
-
-func emitGeneratedSourceListForGN(p *Project, fsReaderWriter oswrapper.FilesystemReaderWriter, generatedSources []string, depsForGeneratedSources []string) error {
-	gniPath := path.Join(p.Root, "generated_sources.gni")
-	sb := &strings.Builder{}
-	sb.WriteString(common.Header("", "", "#"))
-	sb.WriteString("\n")
-	sb.WriteString("import(\"tint.gni\")\n\n")
-
-	emitList(sb, generatedSources, "tint_generated_sources = [\n", "]\n\n", "  \"${root_gen_dir}/", "\",\n")
-	emitList(sb, depsForGeneratedSources, "tint_generation_dependencies = [\n", "]\n", "  \"", "\",\n")
-
-	return writeFileIfStale(p, fsReaderWriter, gniPath, sb.String())
 }
 
 func writeFileIfStale(p *Project, fsReaderWriter oswrapper.FilesystemReaderWriter, filepath string, content string) error {
