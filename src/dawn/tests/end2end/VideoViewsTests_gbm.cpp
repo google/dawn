fba3df91f24c2bc1f89f602ae57be2cba26ae4c5
Corentin Wallez <cwallez@chromium.org>
2026-06-01 08:33:22 -0700

    Remove TODO about reordering issue with gbm.h
    It is the only place where gbm.h is included so we can special case that
    x11_with_undefs.h needs to be included with it.
    
    Fixed: 518013184
    Change-Id: I1120f097164b233f775d46fb80e18a4ad2d74235
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/312735
    Reviewed-by: dan sinclair <dsinclair@chromium.org>
    Commit-Queue: Corentin Wallez <cwallez@chromium.org>

--- src/dawn/tests/end2end/VideoViewsTests_gbm.cpp
+++ src/dawn/tests/end2end/VideoViewsTests_gbm.cpp
@@ -26,7 +26,6 @@
 // OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 #include <fcntl.h>
-#include <gbm.h>
 
 #include <cstring>
 #include <memory>
@@ -34,7 +33,11 @@
 #include <utility>
 #include <vector>
 
-// TODO(518013184): Fix include ordering bug
+// gbm.h transitively include X11 headers which have problematic #define for common names (like
+// "Always") preemptively include xlib_with_undefs.h that will do the necessary #undefs before gbm.h
+// tries to include X11.h.
+#include <gbm.h>
+
 #include "src/dawn/common/xlib_with_undefs.h"
 // Comment to prevent reordering.
 
