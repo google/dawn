80a418413aa65086152ca9e758bbe1b40db95529
Brian Sheedy <bsheedy@google.com>
2026-07-29 08:33:59 -0700

    Check that EGL display supports client API
    Updates the EGL display code to check whether the display actually
    supports the requested API (OpenGL vs. OpenGLES). This resolves flaky
    EGL_BAD_PARAMETER errors that get reported when running tests with
    Wayland.
    
    Without this, the failure to create the OpenGL context can surface at
    a later point due to eglGetError() being thread-local. While this
    does not actually appear to affect test behavior, the error logs can
    act as a red herring.
    
    Bug: 539481794
    Change-Id: I34caf7aabbd65ffb326413df4d89dd4fa2596361
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/327797
    Commit-Queue: Stephen White <senorblanco@chromium.org>
    Reviewed-by: Stephen White <senorblanco@chromium.org>
    Auto-Submit: Brian Sheedy <bsheedy@google.com>

--- src/dawn/native/opengl/DisplayEGL.cpp
+++ src/dawn/native/opengl/DisplayEGL.cpp
@@ -30,6 +30,9 @@
 #include <string>
 #include <utility>
 
+#include "absl/container/flat_hash_set.h"
+#include "absl/strings/str_split.h"
+
 namespace dawn::native::opengl {
 
 // static
@@ -111,6 +114,18 @@ MaybeError DisplayEGL::InitializeWithProcAndDisplay(EGLGetProcProc getProc, EGLD
     DAWN_INVALID_IF(
         egl->GetMajorVersion() < 1 || (egl->GetMajorVersion() == 1 && egl->GetMinorVersion() < 4),
         "EGL version (%u.%u) must be at least 1.4", egl->GetMajorVersion(), egl->GetMinorVersion());
+
+    // Verify that the EGL display supports the requested client API. While this is not strictly
+    // necessary, it does prevent flaky EGL_BAD_PARAMETER logs which can be red herrings when
+    // debugging test failures.
+    const char* rawClientAPIs = egl->QueryString(mDisplay, EGL_CLIENT_APIS);
+    if (rawClientAPIs != nullptr) {
+        absl::flat_hash_set<std::string_view> clientAPIs = absl::StrSplit(rawClientAPIs, " ");
+        const char* targetAPI = (mApiEnum == EGL_OPENGL_API) ? "OpenGL" : "OpenGL_ES";
+        DAWN_INVALID_IF(!clientAPIs.contains(targetAPI),
+                        "EGL display does not support requested client API '%s' (supported: %s).",
+                        targetAPI, rawClientAPIs);
+    }
 
     return {};
 }
