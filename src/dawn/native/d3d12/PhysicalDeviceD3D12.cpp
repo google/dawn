43f7556071ab3f11117f48423e20ecaf318abc3e
dan sinclair <dsinclair@chromium.org>
2026-09-17 19:58:49 -0700

    [dawn] Update some D3D validation errors to internal.
    Update the error type for resource table with the wrong binding tier and
    non-gen 7 Intel from Validation to Internal.
    
    Bug: 536639352
    Change-Id: I8f063af61e3e37b7c46916fcb7e5f29c2616643a
    Reviewed-on: https://dawn-review.googlesource.com/c/dawn/+/342495
    Reviewed-by: Loko Kung <lokokung@google.com>
    Commit-Queue: dan sinclair <dsinclair@chromium.org>
    Reviewed-by: Corentin Wallez <cwallez@chromium.org>

--- src/dawn/native/d3d12/PhysicalDeviceD3D12.cpp
+++ src/dawn/native/d3d12/PhysicalDeviceD3D12.cpp
@@ -305,7 +305,7 @@ MaybeError PhysicalDevice::InitializeSupportedLimitsImpl(CombinedLimits* limits)
 
     if (featureLevels.MaxSupportedFeatureLevel == D3D_FEATURE_LEVEL_11_0 &&
         featureData.ResourceBindingTier < D3D12_RESOURCE_BINDING_TIER_2) {
-        return DAWN_VALIDATION_ERROR(
+        return DAWN_INTERNAL_ERROR(
             "At least Resource Binding Tier 2 is required for D3D12 Feature Level 11.0 "
             "devices.");
     }
@@ -1004,7 +1004,7 @@ MaybeError PhysicalDevice::ValidateUseOfD3D12() const {
     // D3D12 is no longer allowed on 4th Generation Intel Processor Graphics.
     // https://www.intel.com/content/www/us/en/support/articles/000057520/graphics.html
     if (gpu_info::IsIntelGen7(vendorId, deviceId)) {
-        return DAWN_VALIDATION_ERROR("D3D12 backend is not allowed on Intel gen-7 GPUs.");
+        return DAWN_INTERNAL_ERROR("D3D12 backend is not allowed on Intel gen-7 GPUs.");
     }
 
     return {};
